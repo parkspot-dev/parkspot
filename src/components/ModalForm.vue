b026b68b68fd6cc4cbdf858cf62cad2f9d075f3b
iamfiasco <iamfiascoiv@gmail.com>
2020-12-19 11:54:49 +0530

    failed await testingVII

--- src/components/ModalForm.vue
+++ src/components/ModalForm.vue
@@ -63,12 +63,12 @@ export default{
 		    method: 'POST', // *GET, POST, PUT, DELETE, etc.
 		    mode: 'no-cors', // no-cors, *cors, same-origin
 		    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-		    credentials: 'include', // include, *same-origin, omit
+		    credentials: 'omit', // include, *same-origin, omit
 		    headers: {
 		      'Content-Type': 'application/json'
 		    },
 		    redirect: 'follow', // manual, *follow, error
-		    referrerPolicy: 'origin', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+		    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
 		    body: JSON.stringify(data) // body data type must match "Content-Type" header
 		  });
 		  return response.json(); // parses JSON response into native JavaScript objects
