8b959d6fbaa0da08d3913d4e064490e57b7a50a6
iamfiasco <iamfiascoiv@gmail.com>
2020-12-19 11:58:28 +0530

    failed await testingVIII

--- src/components/ModalForm.vue
+++ src/components/ModalForm.vue
@@ -61,13 +61,10 @@ export default{
 		  this.isLoading = true
 		  const response = await fetch(url, {
 		    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-		    mode: 'no-cors', // no-cors, *cors, same-origin
-		    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-		    credentials: 'omit', // include, *same-origin, omit
-		    headers: {
-		      'Content-Type': 'application/json'
+		    mode:"cors",
+			headers: {
+		      'Content-Type': 'application/json',
 		    },
-		    redirect: 'follow', // manual, *follow, error
 		    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
 		    body: JSON.stringify(data) // body data type must match "Content-Type" header
 		  });
