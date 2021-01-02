d2e95054b24386a76cfbd5b38600018b5130293c
arrogant-ant <sudhanshuambastha@gmail.com>
2021-01-02 09:32:14 +0530

    Updated About text

--- src/pages/home/components/PSAbout.vue
+++ src/pages/home/components/PSAbout.vue
@@ -7,10 +7,16 @@
 				.column.is-half
 					h1.title.has-text-light About ParkSpot
 					br
-					p.subsitle.has-text-light
+					p.subtitle.has-text-light
 						| We at ParkSpot strive to solve the trivial problem of finding/booking 
 						| available parking slots in vicinity which is faced by millions of people on a daily basis. 
 						| we vie for solutions which brings technology and life closer and accessible.
+					br
+					p.subtitle.has-text-light
+						| ParkSpot is part of this new initiative and incorporates various features aimed at improving
+						| the parking experience for residents and visitors.
+						| These will include removing the need for entry barriers at car park entrances and exits for a seamless flow of traffic,
+						| as vehicle plates will be registered via cameras.
 					br
 					p.has-text-white.has-text-weight-semibold
 						| "When the need for something becomes imperative, you are forced to find ways of getting or achieving it. - lexico.com"
