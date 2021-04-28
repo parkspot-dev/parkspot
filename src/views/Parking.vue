53dc495c429c05106487914ccd423ec936fb3ca5
Sujeet <sujits32@gmail.com>
2021-04-28 15:43:15 +0530

    map image changes to static

---
+++ src/views/Parking.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="app">
+		<PSNavbar />
+        <PSParking />
+		<PSFooter />
+	</div>
+</template>
+<script>
+import PSFooter from '@/components/PSFooter.vue';
+import PSNavbar from '@/components/PSNavbar.vue'
+import PSParking from '../pages/Parking/components/PSParking.vue'
+export default {
+    name: 'Parking',
+    components:{
+        PSNavbar,
+        PSFooter,
+        PSParking
+    }
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    pluginOptions: {
+        sitemap: {
+            urls: [
+                'https://www.parkspot.in/',
+                'https://www.parkspot.in/faq',
+                'https://www.parkspot.in/thanks',
+                'https://www.parkspot.in/srp',
+                'https://www.parkspot.in/blog/eliminating-reason-for-traffic-jam',
+                
+            ]
+        }
+    }
+}
