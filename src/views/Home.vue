96a6464fc371b5f1fcc773c9dabd43da4ff0ae28
Sujeet <sujits32@gmail.com>
2021-05-02 00:01:06 +0530

    lazy-hydration removed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,45 +2,27 @@
   <div id="app">
     <PSNavbar />
     <PSMap />
-    <LazyHydrate when-visible>
+
       <MainBanner />
-    </LazyHydrate>
-    <LazyHydrate when-visible>
       <Services />
-    </LazyHydrate>
-    <LazyHydrate when-visible>
       <PSTeam />
-    </LazyHydrate>
-    <LazyHydrate when-visible>
       <PSOffering />
-    </LazyHydrate>
-    <LazyHydrate when-visible>
       <PSSites />
-    </LazyHydrate>
-    <LazyHydrate when-visible>
       <PSAbout />
-    </LazyHydrate>
-    <LazyHydrate when-visible>
       <contact />
-    </LazyHydrate>
-    <LazyHydrate when-visible>
       <PSFooter />
-    </LazyHydrate>
-  </div>
+   </div>
 </template>
 
 <script>
 import PSNavbar from "@/components/PSNavbar.vue";
 import PSMap from "@/components/MapboxSearch.vue";
-import LazyHydrate from "vue-lazy-hydration";
 
 export default {
   name: "App",
   components: {
-    LazyHydrate,
     PSNavbar,
     PSMap,
-
     MainBanner: () => import("@/pages/home/components/MainBanner.vue"),
     PSTeam: () => import("@/pages/home/components/PSTeam.vue"),
     Services: () => import("@/pages/home/components/Service.vue"),
