e1befefc070bfd971d949c1809755145e0c6fe98
Sujeet <sujits32@gmail.com>
2021-05-09 16:08:36 +0530

    tnc modified for build

--- src/components/PSFooter.vue
+++ src/components/PSFooter.vue
@@ -1,11 +1,10 @@
  <template >
-
  <div class="footer footer has-background-black">
 	 <div class=" content has-text-centered">
 		 <div class="container is-fluid">
 			 <div class="columns is-vcentered is-centered is-multiline px-6 mx-6">
 				 <div class=" column center is-12-mobile is-4" v-for="(elem, i) in bottomicons" :key="i">
-					 <a :href="`${elem.href}`"  alt="social handle icons" v-lazyload>
+					 <a :href="elem.href"  alt="social handle icons" v-lazyload>
 						 <img :data-url="elem.image" class="footericons">
 					 </a>
 				 </div>
@@ -21,26 +20,7 @@
 			 </p>
 		 </div>
 	 </div>
-	 
  </div>
-	<!-- footer.footer.has-background-black
-		.content.has-text-centered
-			.container.is-fluid
-				.columns.is-vcentered.is-centered.is-multiline.px-6.mx-6
-					.column.center.is-12-mobile.is-4(v-for="(elem, i) in bottomicons" :key="i")
-						a(:href="`${elem.href}`" alt="social handle icons" v-lazyload)
-							img(class="footericons" :data-url="elem.image")
-					
-			br
-			a(tag="router-link" :to="{ name: 'Terms' }") link
-			br
-			br
-			p.has-text-white
-				|© 2020 
-				strong.has-text-white The Thought Engine 
-				|- All rights reserved.
-
-		router-view -->
 </template>
 <script>
 	export default{
