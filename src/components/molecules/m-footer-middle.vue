d0006f6d92578dce5e392b86c9aec45960beb613
Sujeet <sujits32@gmail.com>
2021-05-29 12:02:04 +0530

    before boxicons

--- src/components/molecules/m-footer-middle.vue
+++ src/components/molecules/m-footer-middle.vue
@@ -1,21 +1,23 @@
 <template>
-<div class="m_footer_middle">
-<atom-text :text="text1"/>
-    <atom-text :text="text2"/>
-</div>
-    
+  <div class="m_footer_middle">
+    <atom-text :text="text1" />
+    <atom-img :src="src" />
+    <atom-text :text="text2" />
+  </div>
 </template>
 
 <script>
-import atomText from '@/components/atoms/atom-text/atom-text.vue'
+import atomText from "@/components/atoms/atom-text/atom-text.vue";
+import AtomImg from '@/components/atoms/atom-img/atom-img.vue';
 export default {
-  components: { atomText },
-    name:'m-footer-middle',
-    data(){
-        return{
-            text1:"Now Available on",
-            text2:"Follow us"
-        }
-    }
-}
+  components: { atomText,AtomImg },
+  name: "m-footer-middle",
+  data() {
+    return {
+      text1: "Now Available on",
+      text2: "Follow us",
+      src: require('@/assets/img/google-play-badge.png')
+    };
+  },
+};
 </script>
