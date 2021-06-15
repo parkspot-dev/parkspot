4bedf89b07958b21a722a9fde085f2b47a6afa7f
Sujeet <sujits32@gmail.com>
2021-06-15 13:45:07 +0530

    add: number validation in contact added

--- src/components/organisms/o-contact-right.vue
+++ src/components/organisms/o-contact-right.vue
@@ -32,6 +32,7 @@
         :class="{ 'is-loading': toggle }"
         :text="submit"
       />
+      <p v-if="errors.error" class="has-text-danger	">{{errors.text}}</p>
     </form>
   </div>
 </template>
@@ -45,6 +46,7 @@ export default {
   name: "o-contact-right",
   data() {
     return {
+      // input details
       text: "text",
       email: "email",
       tel: "tel",
@@ -54,17 +56,32 @@ export default {
       placeholder4: "Your Questions...",
       submit: "Submit",
       required: true,
+
       toggle: false, // toggle used for showing loading animation of button
+      //contact details object
       userContact: {
         name: "",
         email: "",
         mno: "",
         msg: "",
       },
+      // errors object for validating phone number
+      errors:{
+        error:false,
+        text:"",
+      }
     };
   },
   methods: {
     async onSubmit() {
+      // validation check
+      const check = parseInt(this.userContact.mno)
+      if(isNaN(check)){
+        this.errors.error=true
+        this.errors.text="Note:Please enter a valid Mobile No.!!"
+        this.userContact.mno = "";
+        return
+      }
       this.toggle = !this.toggle;
       const user = JSON.parse(JSON.stringify(this.userContact)); // getting proper object after stringify and parse
       const res = await fetch("https://maya.parkspot.in/contact", {
@@ -88,6 +105,7 @@ export default {
       this.userContact.mno = "";
       this.userContact.msg = "";
       this.toggle = !this.toggle;
+      this.errors.error=false //removing error msg
       this.submit = "Thank You for Contacting Us!"; // changing the text of submit button after submitting the form
     },
   },
