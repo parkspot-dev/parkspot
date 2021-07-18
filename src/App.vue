e0b3e6a46ea56cb326287983cf20c4f82946dd4c
nextishwar <nextishwar@gmail.com>
2021-07-18 15:49:06 +0530

    written some changes

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <!-- :key="$route.fullPath" temporary fix ...as its only problem with this method is that every path in that router will update in the case of a path change. You may not want all that needless re-rendering, but that’s a decision for you to make.  -->
     <t-footer />
   </div>
-</template>
+some changes
 
 
 <script>
