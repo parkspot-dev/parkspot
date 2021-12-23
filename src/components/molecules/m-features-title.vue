a8526eace36f1926cf3a1130c91577256da005cd
Sujeet <sujits32@gmail.com>
2021-12-23 23:46:40 +0530

    fixed search bug and added new features page

---
+++ src/components/molecules/m-features-title.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="m-features-title">
+    <h1 class="title--size">
+      Why Parking With ParkSpot Is <br />
+      <span class="title__second">GOOD</span>
+    </h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "m-features-title",
+};
+</script>
+<style scoped>
+.title--size {
+  font-size: 2rem;
+}
+.title__second {
+  font-size: 1.5em;
+  color: #0085ad;
+  font-weight: 700;
+}
+</style>
