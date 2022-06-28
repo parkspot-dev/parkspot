dede4ee3b7a3fc7c2f6f4560d93bd83f83860fe1
Sujeet Kumar Manjhi <sujits32@gmail.com>
2022-06-28 23:46:45 +0530

    all new atom and molecule component added

---
+++ src/components/molecules/MoleculeCheckbox.vue
@@ -0,0 +1,38 @@
+<template>
+  <ValidationProvider
+    ref="provider"
+    :name="fieldName"
+    :rules="rules"
+    v-slot="{ errors }"
+  >
+    <AtomCheckboxs :values="values"></AtomCheckboxs>
+    <span class="has-text-danger">{{ errors[0] }}</span>
+  </ValidationProvider>
+</template>
+
+<script>
+import { ValidationProvider } from "vee-validate";
+import AtomCheckboxs from "../atoms/AtomCheckboxs.vue";
+export default {
+  name: "MoleculeCheckBox",
+  components: {
+    ValidationProvider,
+    AtomCheckboxs,
+  },
+  props: {
+    rules: {
+      type: [Object, String],
+      default: "",
+    },
+    fieldName: {
+      type: String,
+      required: true,
+    },
+    values: {
+      type: Array,
+    },
+  },
+};
+</script>
+
+<style></style>
