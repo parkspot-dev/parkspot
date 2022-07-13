d28f58789b71c9b1077a44285264a829fd27d5c6
Sujeet Kumar Manjhi <sujits32@gmail.com>
2022-07-13 18:17:14 +0530

    new contact us component added

---
+++ src/components/atoms/AtomTextarea.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-field :label="label">
+    <b-input
+      :maxlength="maxlength"
+      type="textarea"
+      :placeholder="placeholder"
+    ></b-input>
+  </b-field>
+</template>
+
+<script>
+export default {
+  name: "AtomTextarea",
+  props: {
+    /**
+     * Textarea used for long input
+     */
+    label: {
+      type: String,
+      default: "Message",
+    },
+    maxlength: {
+      type: Number,
+      default: 200,
+    },
+    placeholder: {
+      type: String,
+      default: "Message",
+    },
+  },
+};
+</script>
+
+<style></style>
