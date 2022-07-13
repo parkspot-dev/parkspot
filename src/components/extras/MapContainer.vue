4041c9c8d3554c2a34775584273303612b7d1c3f
Sujeet Kumar Manjhi <sujits32@gmail.com>
2022-07-13 12:01:40 +0530

    map added to SO portal and tnc added

---
+++ src/components/extras/MapContainer.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="map" style="min-height: 450px"></div>
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl";
+import { mapGetters, mapMutations } from "vuex";
+export default {
+  name: "MapContainer",
+  props: {
+    /**
+     * map drag option for form only
+     */
+    drag: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ["location"],
+  data() {
+    return {
+      accessToken: process.env.VUE_APP_MAP_ACCESS_TOKEN,
+      img: require("@/assets/img/pstopmini.png"),
+      map: null, // map for mapbox
+      marker: null, // marker for location
+    };
+  },
+  computed: {
+    ...mapGetters({
+      mapConfig: "map/getMapConfig",
+      mapCenter: "map/getNewMapCenter",
+    }),
+  },
+  watch: {
+    mapCenter(newCenter) {
+      this.reCenterMap(newCenter);
+    },
+  },
+  mounted() {
+    mapboxgl.accessToken = this.accessToken;
+
+    // create map
+    this.map = new mapboxgl.Map(this.mapConfig);
+
+    // create the popup
+    const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+      "Your current location."
+    );
+
+    // create DOM element for the marker
+    var markerElement = document.createElement("div");
+    markerElement.className = "marker";
+    markerElement.style.backgroundImage = "url(" + this.img + ")";
+    markerElement.style.width = "50px";
+    markerElement.style.height = "50px";
+    markerElement.style.backgroundSize = "110%";
+
+    // create the marker
+    this.marker = new mapboxgl.Marker(markerElement, {
+      draggable: this.drag,
+    })
+      .setLngLat(this.mapConfig.center)
+      .setPopup(popup)
+      .addTo(this.map);
+    if (this.drag) {
+      this.map.on("click", (e) => {
+        this.marker.setPopup(popup).setLngLat(e.lngLat).addTo(this.map);
+        this.updateMapConfig(this.marker.getLngLat());
+        this.$emit("location", this.marker.getLngLat());
+      });
+      this.marker.on("dragend", () => {
+        this.updateMapConfig(this.marker.getLngLat());
+        this.$emit("location", this.marker.getLngLat());
+      });
+    }
+  },
+  methods: {
+    ...mapMutations({
+      updateMapConfig: "map/update-map-config",
+    }),
+    reCenterMap(center) {
+      this.map.flyTo({
+        center: center,
+      });
+      this.marker.setLngLat(center);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#map {
+  width: 100%;
+}
+</style>
---
+++ src/components/organisms/OrganismAddressForm.vue
@@ -0,0 +1,86 @@
+<template>
+  <ValidationObserver ref="observer" v-slot="{}">
+    <ValidationProvider
+      name="location"
+      rules="required"
+      v-slot="{ validate, errors }"
+    >
+      <SearchInput @change.native="validate" class="mb-4"></SearchInput>
+      <span>{{ errors[0] }}</span>
+    </ValidationProvider>
+    <ValidationProvider
+      name="map"
+      rules="required"
+      v-slot="{ validate, errors }"
+    >
+      <AtomParagraph
+        >Note: Drag the marker to pin the exact location.</AtomParagraph
+      >
+      <MapContainer :drag="true" @location="validate"></MapContainer>
+      <span>{{ errors[0] }}</span>
+    </ValidationProvider>
+  </ValidationObserver>
+</template>
+
+<script>
+import SearchInput from "../extras/SearchInput.vue";
+import MapContainer from "../extras/MapContainer.vue";
+import AtomParagraph from "../atoms/AtomParagraph.vue";
+import { ValidationProvider, ValidationObserver } from "vee-validate";
+import { mapGetters, mapMutations } from "vuex";
+export default {
+  name: "OrganismAddressForm",
+  components: {
+    SearchInput,
+    MapContainer,
+    AtomParagraph,
+    ValidationProvider,
+    ValidationObserver,
+  },
+  props: {
+    formSubmitted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ["formValidate"],
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters({
+      locWithLatLng: "map/getLocWithLatLng",
+    }),
+  },
+  watch: {
+    formSubmitted(newVal) {
+      console.log("address form");
+      if (newVal) {
+        this.$refs.observer
+          .validate()
+          .then((el) => {
+            if (el) {
+              this.$emit("formValidate", el);
+              this.submit();
+            } else {
+              this.$emit("formValidate", el);
+            }
+          })
+          .catch((er) => {
+            console.log(er);
+          });
+      }
+    },
+  },
+  methods: {
+    ...mapMutations({
+      updateLocationDetails: "soportal/update-location-details",
+    }),
+    submit() {
+      this.updateLocationDetails(this.locWithLatLng);
+    },
+  },
+};
+</script>
+
+<style></style>
