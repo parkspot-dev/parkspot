78673b8740a5a4192607ffbb23d775a782779ec2
Sujeet Kumar Manjhi <sujits32@gmail.com>
2022-07-14 14:06:27 +0530

    new our services in development

--- src/views/temp.vue
+++ src/views/temp.vue
@@ -1,14 +1,14 @@
 <template>
-  <TemplateHomeBanner></TemplateHomeBanner>
+  <TemplateOurProducts></TemplateOurProducts>
 </template>
 <script>
-import TemplateHomeBanner from "../components/templates/TemplateHomeBanner.vue";
+import TemplateOurProducts from "../components/templates/TemplateOurProducts.vue";
 export default {
   /**
    * This page is used for development testing purposes.
    */
   name: "Temp",
-  components: { TemplateHomeBanner },
+  components: { TemplateOurProducts },
   data() {
     return {};
   },
