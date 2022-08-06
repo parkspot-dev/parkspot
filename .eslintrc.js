522d492e5e165266f893b2a4ca9e4df6ee9b3405
Sujeet Kumar Manjhi <sujits32@gmail.com>
2022-08-06 14:34:04 +0530

    eslint changed

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  // extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/essential",
+    "prettier",
+  ],
   parserOptions: {
     parser: "babel-eslint",
   },
