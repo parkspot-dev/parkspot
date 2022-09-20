256c36dc898f78dedee72412d440c6425594156c
Sujeet Kumar Manjhi <sujits32@gmail.com>
2022-09-20 17:23:00 +0530

    pagination size change

--- src/components/extras/PaginationBody.vue
+++ src/components/extras/PaginationBody.vue
@@ -3,7 +3,7 @@
         <b-pagination
             :total="totals"
             v-model="currentPage"
-            :size="'is-small'"
+            :size="'is-medium'"
             :per-page="3"
             :range-before="1"
             :range-after="1"
@@ -11,25 +11,6 @@
             :order="'is-centered'"
             @change="onChange"
         >
-            <template #previous="props">
-                <b-pagination-button
-                    :page="props.page"
-                    tag="button"
-                    class="pagination-btn"
-                >
-                    Previous
-                </b-pagination-button>
-            </template>
-
-            <template #next="props">
-                <b-pagination-button
-                    :page="props.page"
-                    tag="button"
-                    class="pagination-btn"
-                >
-                    Next
-                </b-pagination-button>
-            </template>
         </b-pagination>
     </section>
 </template>
@@ -70,26 +51,4 @@ export default {
 </script>
 
 <style >
-.pagination-link.is-current {
-    background-color: var(--secondary-color) !important;
-    border-color: var(--secondary-color);
-}
-
-.pagination-btn {
-    background-color: var(--primary-color);
-    border-color: var(--primary-color);
-    cursor: pointer;
-}
-
-.pagination-btn:visited {
-    border-color: var(--primary-color);
-}
-
-.pagination-btn:hover {
-    border-color: var(--primary-color);
-}
-
-.pagination-btn:active {
-    border-color: var(--primary-color);
-}
 </style>
