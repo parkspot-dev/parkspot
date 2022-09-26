f1a15dc29667bdc940708b7879f27ab541ec71fc
Sujeet Kumar Manjhi <sujits32@gmail.com>
2022-09-26 23:25:06 +0530

    Srp card (#183)
    * srp card design change
    
    * srp card design2
    
    * animation added
    
    * srp card design3
    
    * PR comment fix

--- src/components/molecules/MoleculeSRPCard.vue
+++ src/components/molecules/MoleculeSRPCard.vue
@@ -1,33 +1,41 @@
 <template>
     <div class="custom-card">
-        <figure class="card-img image is-96x96">
-            <img :alt="'Parking spot image'" :src="spot.IconURL" />
-        </figure>
-        <AtomRating class="card-rating" :rate="spot.Rating"></AtomRating>
         <AtomParagraph class="card-title" :type="'span'">
-            {{ spot.Name }}
+            <strong> {{ spot.Name }} </strong>
         </AtomParagraph>
         <div class="card-location">
             <span class="location-icon">
                 <AtomIcon :icon="'map-marker-radius'"></AtomIcon>
             </span>
-            <AtomParagraph class="location-text" :type="'span'">
+            <AtomParagraph
+                class="location-text"
+                :variation="'small'"
+                :type="'span'"
+            >
                 {{ spot.Address }}
             </AtomParagraph>
         </div>
-        <AtomParagraph class="card-distance" :type="'span'">
-            <strong>Distance : </strong> {{ spot.Distance }} KM
-        </AtomParagraph>
-        <AtomParagraph class="card-type" :type="'span'">
-            <strong>Type : </strong> {{ spot.VehicleType }}
-        </AtomParagraph>
+        <AtomRating class="card-rating" :rate="spot.Rating"></AtomRating>
+        <div class="card-distance">
+            <AtomParagraph class="distance-text" :type="'p'">
+                <span class="text-color"> {{ spot.Distance }} KM </span>
+            </AtomParagraph>
+        </div>
+
         <AtomParagraph class="card-rate" :type="'span'">
-            <strong>Rate : </strong> ₹ {{ spot.Rate }} /
-            {{ spot.RentUnit }}
+            <span class="rate-icon">
+                <AtomIcon :icon="'tag'"></AtomIcon>
+            </span>
+            <span> ₹ {{ spot.Rate }}/{{ spot.RentUnit }} </span>
         </AtomParagraph>
-        <AtomParagraph class="card-spot" :type="'span'">
-            <strong>Available Spot : </strong>
-            {{ spot.SlotsAvailable | available }}
+        <AtomParagraph class="card-spot" :type="'p'">
+            <span
+                v-if="getAvailability(spot.SlotsAvailable)"
+                class="card-spot-green"
+            >
+                Available
+            </span>
+            <span v-else class="card-spot-red"> Rented Out </span>
         </AtomParagraph>
         <AtomButton class="card-btn" @click.native="onBook">
             Book Spot
@@ -54,18 +62,15 @@ export default {
         },
     },
     emits: ['booked'],
-    filters: {
-        available(slotAvailable) {
-            if (slotAvailable === 0) {
-                return 'N/A';
-            } else {
-                return slotAvailable;
-            }
-        },
-    },
     methods: {
         onBook() {
             this.$emit('booked');
+        },
+        getAvailability(spot) {
+            if (spot > 0) {
+                return true;
+            }
+            return false;
         },
     },
 };
@@ -76,30 +81,26 @@ export default {
     border-radius: var(--border-default);
     box-shadow: 0 0.5em 1em -0.125em rgb(10, 10, 10, 10%),
         0 0 0 1px rgb(10, 10, 10, 20%);
-    column-gap: 0.75rem;
+    column-gap: 0.25rem;
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    grid-template-rows: repeat(6, auto);
+    grid-template-columns: repeat(6, auto);
+    grid-template-rows: repeat(5, auto);
     margin: 0.5rem;
-    max-width: 100%;
+    max-width: 500px;
     overflow: hidden;
-    padding: 2rem;
-    row-gap: 0.25rem;
+    padding: 1rem;
+    border: 2px solid transparent;
+    transition: transform 0.3s ease;
 }
 
-.card-img {
-    grid-column: 1 / 1;
-    grid-row: 1 / 5;
+.custom-card:hover {
+    border: 2px solid var(--secondary-color);
+    transform: scale(1.03);
 }
-
 .card-title {
-    font-weight: var(--bold-font);
-    grid-column: 2 / 6;
+    font-size: 1rem;
+    grid-column: 1 / 5;
     grid-row: 1 / 1;
-}
-
-.card-btn {
-    grid-column: 2 / 6;
 }
 
 .card-location {
@@ -107,9 +108,11 @@ export default {
     display: flex;
     font-size: var(--sp-size-sm);
     gap: 10px;
-    grid-column: 2 / 6;
+    grid-column: 1 / 5;
     grid-row: 2 / 2;
     position: relative;
+    line-height: 1.1;
+    color: #6b6b6b;
 }
 
 .location-icon {
@@ -124,24 +127,65 @@ export default {
 }
 
 .card-rating {
-    grid-row: 5 / 5;
-    margin: auto;
+    grid-column: 1 / 3;
+    grid-row: 3 / 3;
+    margin-top: 0.75rem;
 }
 
-.card-distance,
+.card-distance {
+    background-color: rgb(214, 253, 255);
+    width: 58px;
+    text-align: center;
+    padding: 0.75rem 0.75rem;
+    border-radius: var(--border-default);
+    grid-column: 5 / 7;
+    grid-row: 1 / 3;
+    align-self: end;
+    justify-self: end;
+}
+
+.distance-text {
+    line-height: var(--lh-small);
+    font-weight: var(--bold-font);
+    font-size: 16px;
+}
+
+.text-color {
+    color: var(--secondary-color);
+}
+
 .card-rate {
-    grid-column: 2 / 4;
+    align-items: center;
+    display: flex;
+    font-weight: var(--bold-font);
+    gap: 10px;
+    grid-column: 1 / 3;
+    grid-row: 4 / 5;
+    margin-bottom: 0.75rem;
 }
 
-.card-type,
-.card-spot {
-    grid-column: 4 / 6;
+.rate-icon {
+    color: green;
 }
 
-.card-distance,
-.card-type,
-.card-rate,
 .card-spot {
-    font-size: var(--sp-size-sm);
+    grid-column: 3 / 5;
+    grid-row: 4 / 5;
+}
+
+.card-spot-red {
+    color: red;
+    font-weight: var(--bold-font);
+}
+
+.card-spot-green {
+    color: green;
+    font-weight: var(--bold-font);
+}
+
+.card-btn {
+    grid-column: 1 / 7;
+    grid-row: 5 / 6;
+    font-weight: var(--bold-font);
 }
 </style>
