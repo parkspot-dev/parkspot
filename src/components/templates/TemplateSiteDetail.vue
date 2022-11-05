eef7975c3b4d7675df708260772c15169b12cc21
Sujeet Kumar Manjhi <sujits32@gmail.com>
2022-11-05 09:09:49 +0530

    pdp files name changed

--- src/components/templates/TemplateSiteDetail.vue
+++ src/components/templates/TemplateSiteDetail.vue
@@ -2,12 +2,12 @@
     <BodyWrapper>
         <!-- collage for image gallery Organism-->
         <!-- Rate Card Organism -->
-        <div class="pdp-main-description">
+        <div class="site-detail-main-description">
             <h1>Spot Name</h1>
             <p>Address of the Spot in Banglore, Pin 876543, Karnataka, India</p>
         </div>
 
-        <div class="pdp-amenities">
+        <div class="site-detail-amenities">
             <h2>What this place offers</h2>
             <ul>
                 <li>Covered</li>
@@ -17,23 +17,23 @@
             </ul>
         </div>
 
-        <div class="pdp-map">
+        <div class="site-detail-map">
             <h2>How to get here?</h2>
         </div>
 
-        <div class="pdp-things">
+        <div class="site-detail-things">
             <!-- Organism to show the graphical layout -->
         </div>
 
         <!-- only to admin -->
-        <div class="pdp-owner"></div>
+        <div class="site-detail-owner"></div>
     </BodyWrapper>
 </template>
 
 <script>
 import BodyWrapper from '../extras/BodyWrapper.vue';
 export default {
-    name: 'TemplatePdp',
+    name: 'TemplateSiteDetail',
     components: {
         BodyWrapper,
     },
