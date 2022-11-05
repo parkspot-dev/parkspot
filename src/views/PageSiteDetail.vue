8d818386b55f76725f0f70b1971535a32a2e9b20
Sujeet Kumar Manjhi <sujits32@gmail.com>
2022-11-05 09:09:49 +0530

    pdp files name changed

---
+++ src/views/PageSiteDetail.vue
@@ -0,0 +1,13 @@
+<template>
+    <TemplateSiteDetail></TemplateSiteDetail>
+</template>
+
+<script>
+import TemplateSiteDetail from '../components/templates/TemplateSiteDetail.vue';
+export default {
+    name: 'PageSiteDetail',
+    components: {
+        TemplateSiteDetail,
+    },
+};
+</script>
