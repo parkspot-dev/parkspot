3aea63ed9599193a200bb225109a531bb1f1cc97
Sujeet Kumar Manjhi <sujits32@gmail.com>
2022-11-18 22:41:06 +0530

    map marker for selected spot in sdp page

--- src/components/templates/TemplateSrp.vue
+++ src/components/templates/TemplateSrp.vue
@@ -37,7 +37,6 @@ import PaginationBody from '../extras/PaginationBody.vue';
 import MoleculeSRPCard from '../molecules/MoleculeSRPCard.vue';
 import MapContainer from '../extras/MapContainer.vue';
 import SearchInput from '../extras/SearchInput.vue';
-import { mapMutations } from 'vuex';
 export default {
     name: 'TemplateSrp',
     components: {
