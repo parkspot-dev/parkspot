7b607d2aa05bb052fe6d601365e6c94819ebc5de
Sujeet Kumar Manjhi <sujits32@gmail.com>
2022-11-26 11:08:14 +0530

    fallback added to spot images

--- src/components/organisms/OrganismImageGallery.vue
+++ src/components/organisms/OrganismImageGallery.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="gallery-container">
         <div id="lightgallery">
-            <template v-for="i in spotImage.length">
+            <template v-for="(image, i) in spotImage">
                 <a
                     :key="i"
                     class="gallery-item"
                     :class="imageSize"
-                    :href="spotImage"
+                    :href="image"
                     :data-sub-html="`<h4>Photo by - <a href='https://www.parkspot.in' >Parkspot </a></h4><p> Location - ${locationName}</p>`"
                 >
-                    <img class="img-responsive" :src="spotImage" />
+                    <img class="img-responsive" :src="image" />
                 </a>
             </template>
         </div>
