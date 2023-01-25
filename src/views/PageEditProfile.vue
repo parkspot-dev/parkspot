1004f53a4f230b0fd9e66e6934713846f2ef053d
Sujeet Kumar Manjhi <sujits32@gmail.com>
2023-01-25 15:10:41 +0530

    edit page added

--- src/views/PageEditProfile.vue
+++ src/views/PageEditProfile.vue
@@ -1,9 +1,15 @@
 <template>
-    <div></div>
+    <div>
+        <TemplateEditProfile></TemplateEditProfile>
+    </div>
 </template>
 
 <script>
+import TemplateEditProfile from '../components/templates/TemplateEditProfile.vue';
 export default {
     name: 'PageEditProfile',
+    components: {
+        TemplateEditProfile,
+    },
 };
 </script>
