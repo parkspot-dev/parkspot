534630d7cc6a2c01094f4848aafb37986341362c
Sujeet Kumar Manjhi <sujits32@gmail.com>
2023-02-09 20:58:44 +0530

    removed file not needed

--- src/includes/auth.js
+++
@@ -1,5 +0,0 @@
-import { firebase } from 'firebase';
-import { firebaseui } from 'firebaseui ';
-
-// Initialize the FirebaseUI Widget using Firebase.
-const ui = new firebaseui.auth.AuthUI(firebase.auth());
