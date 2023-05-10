7a862b1dddc70e1158946c8aab8eca555199ae55
Sujeet Kumar Manjhi <sujits32@gmail.com>
2023-05-10 08:57:13 +0530

    filters added

--- src/components/molecules/MoleculeSRPCard.vue
+++ src/components/molecules/MoleculeSRPCard.vue
@@ -1,59 +1,64 @@
 <template>
-    <div class="custom-card">
-        <AtomParagraph class="card-title" :type="'span'">
-            <strong> {{ spot.Name }} </strong>
-        </AtomParagraph>
+    <div class="custom-card" @click="onDetails">
+        <div class="card-title">
+            <p>
+                {{ spot.Name }}
+            </p>
+        </div>
+
         <div class="card-location">
             <span class="location-icon">
                 <AtomIcon :icon="'map-marker-radius'"></AtomIcon>
             </span>
-            <AtomParagraph
-                class="location-text"
-                :variation="'small'"
-                :type="'span'"
-            >
+            <p class="location-text">
                 {{ spot.Address }}
-            </AtomParagraph>
+            </p>
         </div>
         <AtomRating class="card-rating" :rate="spot.Rating"></AtomRating>
         <div class="card-distance">
-            <AtomParagraph class="distance-text" :type="'p'">
+            <p class="distance-text">
                 <span class="text-color"> {{ spot.Distance }} KM </span>
-            </AtomParagraph>
+            </p>
         </div>
 
-        <AtomParagraph class="card-rate" :type="'span'">
-            <span class="rate-icon">
-                <AtomIcon :icon="'tag'"></AtomIcon>
-            </span>
-            <span> ₹ {{ spot.Rate }}/{{ spot.RentUnit }} </span>
-        </AtomParagraph>
-        <AtomParagraph class="card-spot" :type="'p'">
-            <span
-                v-if="getAvailability(spot.SlotsAvailable)"
-                class="card-spot-green"
-            >
-                Available
-            </span>
-            <span v-else class="card-spot-red"> Rented Out </span>
-        </AtomParagraph>
-        <AtomButton class="card-btn" @click.native="onDetails">
+        <div class="card-rate">
+            <p>
+                <span class="rate-icon">
+                    <AtomIcon :icon="'tag'"></AtomIcon>
+                </span>
+                <span> ₹ {{ spot.Rate }}/{{ spot.RentUnit }} </span>
+            </p>
+        </div>
+
+        <div class="card-spot">
+            <p>
+                <span
+                    v-if="getAvailability(spot.SlotsAvailable)"
+                    class="card-spot-green"
+                >
+                    Available
+                </span>
+                <span v-else class="card-spot-red"> Rented Out </span>
+            </p>
+        </div>
+
+        <div class="card-active-border-left"></div>
+        <div class="card-active-border-right"></div>
+        <!-- <AtomButton class="card-btn" @click.native="onDetails">
             Spot Details
-        </AtomButton>
+        </AtomButton> -->
     </div>
 </template>
 
 <script>
 import AtomRating from '../atoms/AtomRating.vue';
-import AtomParagraph from '../atoms/AtomParagraph.vue';
-import AtomButton from '../atoms/AtomButton.vue';
+// import AtomButton from '../atoms/AtomButton.vue';
 import AtomIcon from '../atoms/AtomIcon.vue';
 export default {
     name: 'MoleculeSRPCard',
     components: {
         AtomRating,
-        AtomParagraph,
-        AtomButton,
+        // AtomButton,
         AtomIcon,
     },
     props: {
@@ -76,121 +81,146 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .custom-card {
     border-radius: var(--border-default);
     box-shadow: 0 0.5em 1em -0.125em rgb(10, 10, 10, 10%),
         0 0 0 1px rgb(10, 10, 10, 20%);
-    column-gap: 0.25rem;
+    column-gap: 12px;
     display: grid;
     grid-template-columns: repeat(6, 1fr);
-    grid-template-rows: repeat(5, 1fr);
-    height: 220px;
+    grid-template-rows: repeat(4, 1fr);
+    // height: 150px;
     margin: 0.5rem;
     max-width: 500px;
     overflow: hidden;
     padding: 1rem;
-    border: 2px solid transparent;
+    // border: 3px solid transparent;
     transition: transform 0.3s ease;
-}
-
-.custom-card:hover {
-    border: 2px solid var(--secondary-color);
-    transform: scale(1.03);
-}
-.card-title {
-    font-size: 1rem;
-    grid-column: 1 / 5;
-    grid-row: 1 / 1;
-    -webkit-box-orient: vertical;
-    display: -webkit-box;
-    -webkit-line-clamp: 1;
-    overflow: hidden;
-}
-
-.card-location {
-    align-items: center;
-    display: flex;
-    font-size: var(--sp-size-sm);
-    gap: 10px;
-    grid-column: 1 / 5;
-    grid-row: 2 / 2;
+    cursor: pointer;
     position: relative;
-    line-height: 1.1;
-    color: #6b6b6b;
-}
 
-.location-icon {
-    font-size: 20px;
-}
+    &:hover {
+        // border-left: 3px solid var(--secondary-color);
+        // border-right: 3px solid var(--secondary-color);
+        transform: scale(1.03);
+        &::before {
+            content: '';
+            background: var(--secondary-color);
+            position: absolute;
+            bottom: 25%;
+            left: 0;
+            height: 50%;
+            width: 2px;
+        }
+        &::after {
+            content: '';
+            background: var(--secondary-color);
+            position: absolute;
+            bottom: 25%;
+            right: 0;
+            height: 50%;
+            width: 2px;
+        }
+    }
 
-.location-text {
-    -webkit-box-orient: vertical;
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    overflow: hidden;
-}
+    .card-title {
+        font-size: 15px;
+        font-weight: 500;
+        grid-column: 1 / 5;
+        grid-row: 1 / 1;
+        -webkit-box-orient: vertical;
+        display: -webkit-box;
+        -webkit-line-clamp: 1;
+        overflow: hidden;
+        padding-top: 5px;
+        padding-bottom: 5px;
+    }
+    .card-location {
+        align-items: center;
+        display: flex;
+        font-size: 12px;
+        gap: 10px;
+        grid-column: 1 / 5;
+        grid-row: 3 / 3;
+        position: relative;
+        line-height: 1.1;
+        color: #6b6b6b;
 
-.card-rating {
-    grid-column: 1 / 3;
-    grid-row: 3 / 3;
-    margin-top: 0.75rem;
-}
+        .location-icon {
+            font-size: 20px;
+        }
 
-.card-distance {
-    background-color: rgb(214, 253, 255);
-    width: 58px;
-    text-align: center;
-    padding: 0.75rem 0.75rem;
-    border-radius: var(--border-default);
-    grid-column: 5 / 7;
-    grid-row: 1 / 3;
-    align-self: end;
-    justify-self: end;
-}
+        .location-text {
+            -webkit-box-orient: vertical;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            overflow: hidden;
+            font-size: 10px;
+        }
+    }
 
-.distance-text {
-    line-height: var(--lh-small);
-    font-weight: var(--bold-font);
-    font-size: 16px;
-}
+    .card-rating {
+        grid-column: 1 / 3;
+        grid-row: 2 /2;
+        // margin-top: 0.75rem;
+        margin-bottom: unset;
+    }
 
-.text-color {
-    color: var(--secondary-color);
-}
+    .card-distance {
+        background-color: rgb(214, 253, 255);
+        width: 58px;
+        text-align: center;
+        padding: 0.75rem 0.75rem;
+        border-radius: var(--border-default);
+        grid-column: 5 / 7;
+        grid-row: 1 / 3;
+        align-self: end;
+        justify-self: end;
+    }
 
-.card-rate {
-    align-items: center;
-    display: flex;
-    font-weight: var(--bold-font);
-    gap: 10px;
-    grid-column: 1 / 4;
-    grid-row: 4 / 5;
-    margin-bottom: 0.75rem;
-}
+    .distance-text {
+        line-height: var(--lh-small);
+        font-weight: var(--bold-font);
+        font-size: 14px;
+    }
 
-.rate-icon {
-    color: green;
-}
+    .text-color {
+        color: var(--secondary-color);
+    }
 
-.card-spot {
-    grid-column: 4 / 6;
-    grid-row: 4 / 5;
-}
+    .card-rate {
+        align-items: center;
+        display: flex;
+        font-weight: var(--bold-font);
+        gap: 10px;
+        grid-column: 1 / 4;
+        grid-row: 4 / 5;
+    }
 
-.card-spot-red {
-    color: red;
-    font-weight: var(--bold-font);
-}
+    .rate-icon {
+        color: green;
+    }
 
-.card-spot-green {
-    color: green;
-    font-weight: var(--bold-font);
-}
+    .card-spot {
+        grid-column: 4 / 6;
+        grid-row: 4 / 5;
+    }
 
-.card-btn {
-    grid-column: 1 / 7;
-    grid-row: 5 / 6;
-    font-weight: var(--bold-font);
+    .card-spot-red {
+        color: red;
+        font-weight: var(--bold-font);
+    }
+
+    .card-spot-green {
+        color: green;
+        font-weight: var(--bold-font);
+    }
+
+    .card-btn {
+        grid-column: 1 / 7;
+        grid-row: 5 / 6;
+        font-weight: var(--bold-font);
+    }
 }
 </style>
--- src/components/templates/TemplateSrp.vue
+++ src/components/templates/TemplateSrp.vue
@@ -6,23 +6,39 @@
                 @changed="onPageChange"
                 :current="currentPage"
             ></PaginationBody> -->
-            <SearchInput class="map-search" @changed="onChange"></SearchInput>
-            <div>
+            <div class="srp-control">
+                <SearchInput
+                    class="map-search"
+                    @changed="onChange"
+                ></SearchInput>
+                <div class="filter-component">
+                    <b-button icon-left="tune-variant"> Filters </b-button>
+                    <div class="filter-dropdown">
+                        <ul>
+                            <li>Available</li>
+                            <li>Rented</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="srp-results-heading">
                 <p>
-                    <strong>{{ spots.length }} Results </strong> in
-                    {{ selectedLocation.locName }}
+                    <strong>{{ spots.length }} Results </strong>
+                    <span> in {{ selectedLocation.locName }} </span>
                 </p>
             </div>
             <hr />
-            <div class="srp-list-items">
-                <MoleculeSRPCard
-                    v-for="spot in spots"
-                    :key="spot.ID"
-                    :spot="spot"
-                    @on-details="details"
-                    @click.native="selected(spot)"
-                ></MoleculeSRPCard>
-            </div>
+            <keep-alive>
+                <div class="srp-list-items">
+                    <MoleculeSRPCard
+                        v-for="spot in spots"
+                        :key="spot.ID"
+                        :spot="spot"
+                        @on-details="details"
+                        @click.native="selected(spot)"
+                    ></MoleculeSRPCard>
+                </div>
+            </keep-alive>
             <!-- <PaginationBody
                 :totals="totals"
                 @changed="onPageChange"
@@ -88,106 +104,134 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .srp-container {
     display: flex;
     gap: 2rem;
     padding-left: 3rem;
     height: 90vh;
-}
 
-.srp-lists {
-    flex: 20%;
-    padding-top: 2rem;
-    padding-bottom: 2rem;
-}
+    .srp-control {
+        display: flex;
+        gap: 10px;
+        margin-bottom: 24px;
+        margin-top: 10px;
 
-.srp-list-items {
-    padding-top: 1rem;
-    height: 60vh;
-    /* scroll bar width, for use in mask calculations */
-    --scrollbar-width: 8px;
+        .map-search {
+            width: 100%;
+        }
 
-    /* mask fade distance, for use in mask calculations */
-    --mask-height: 32px;
+        .filter-component {
+            position: relative;
+            .filter-dropdown {
+                width: 120px;
+                background-color: white;
+                border: 1px solid black;
+                padding: 5px;
+                position: absolute;
+                z-index: 999;
 
-    /* If content exceeds height of container, overflow! */
-    overflow-y: auto;
+                ul {
+                    font-size: 16px;
+                }
+            }
+        }
+    }
 
-    /* Need to make sure container has bottom space,
-  otherwise content at the bottom is always faded out */
-    padding-bottom: var(--mask-height);
+    .srp-lists {
+        flex: 20%;
+        padding-bottom: 2rem;
 
-    /* Keep some space between content and scrollbar */
-    padding-right: 20px;
+        .srp-results-heading {
+            span {
+                color: rgb(151, 149, 149);
+            }
+        }
 
-    /* The CSS mask */
+        hr {
+            margin-top: 8px;
+        }
+    }
 
-    /* The content mask is a linear gradient from top to bottom */
-    --mask-image-content: linear-gradient(
-        to bottom,
-        transparent,
-        black var(--mask-height),
-        black calc(100% - var(--mask-height)),
-        transparent
-    );
+    .srp-list-items {
+        height: 70vh;
+        /* scroll bar width, for use in mask calculations */
+        --scrollbar-width: 8px;
 
-    /* Here we scale the content gradient to the width of the container
-  minus the scrollbar width. The height is the full container height */
-    --mask-size-content: calc(100% - var(--scrollbar-width)) 100%;
+        /* mask fade distance, for use in mask calculations */
+        --mask-height: 32px;
 
-    /* The scrollbar mask is a black pixel */
-    --mask-image-scrollbar: linear-gradient(black, black);
+        /* If content exceeds height of container, overflow! */
+        overflow-y: auto;
 
-    /* The width of our black pixel is the width of the scrollbar.
-  The height is the full container height */
-    --mask-size-scrollbar: var(--scrollbar-width) 100%;
+        /* Need to make sure container has bottom space,
+      otherwise content at the bottom is always faded out */
+        padding-bottom: var(--mask-height);
 
-    /* Apply the mask image and mask size variables */
-    mask-image: var(--mask-image-content), var(--mask-image-scrollbar);
-    mask-size: var(--mask-size-content), var(--mask-size-scrollbar);
+        /* Keep some space between content and scrollbar */
+        padding-right: 20px;
 
-    /* Position the content gradient in the top left, and the
-  scroll gradient in the top right */
-    mask-position: 0 0, 100% 0;
+        /* The CSS mask */
 
-    /* We don't repeat our mask images */
-    mask-repeat: no-repeat, no-repeat;
-}
+        /* The content mask is a linear gradient from top to bottom */
+        --mask-image-content: linear-gradient(
+            to bottom,
+            transparent,
+            black var(--mask-height),
+            black calc(100% - var(--mask-height)),
+            transparent
+        );
 
-/* custom scroll design */
-::-webkit-scrollbar {
-    width: 5px;
-}
+        /* Here we scale the content gradient to the width of the container
+      minus the scrollbar width. The height is the full container height */
+        --mask-size-content: calc(100% - var(--scrollbar-width)) 100%;
 
-::-webkit-scrollbar-track {
-    box-shadow: inset 0 0 5px rgb(202, 201, 201);
-    border-radius: 10px;
-}
+        /* The scrollbar mask is a black pixel */
+        --mask-image-scrollbar: linear-gradient(black, black);
 
-::-webkit-scrollbar-thumb {
-    background: var(--secondary-color);
-    border-radius: 10px;
-}
+        /* The width of our black pixel is the width of the scrollbar.
+      The height is the full container height */
+        --mask-size-scrollbar: var(--scrollbar-width) 100%;
 
-::-webkit-scrollbar-thumb:hover {
-    background: rgb(0, 88, 114);
-}
+        /* Apply the mask image and mask size variables */
+        mask-image: var(--mask-image-content), var(--mask-image-scrollbar);
+        mask-size: var(--mask-size-content), var(--mask-size-scrollbar);
 
-.srp-map {
-    flex: 50%;
-}
+        /* Position the content gradient in the top left, and the
+      scroll gradient in the top right */
+        mask-position: 0 0, 100% 0;
 
-.map-container {
-    height: 100%;
-    position: relative;
-}
+        /* We don't repeat our mask images */
+        mask-repeat: no-repeat, no-repeat;
+    }
 
-.map-search {
-    /* left: 50%;
-    position: absolute;
-    top: 20%;
-    width: 500px; */
+    /* custom scroll design */
+    ::-webkit-scrollbar {
+        width: 5px;
+    }
+
+    ::-webkit-scrollbar-track {
+        box-shadow: inset 0 0 5px rgb(202, 201, 201);
+        border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: var(--secondary-color);
+        border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background: rgb(0, 88, 114);
+    }
+
+    .srp-map {
+        flex: 50%;
+    }
+
+    .map-container {
+        height: 100%;
+        position: relative;
+    }
 }
 
 @media only screen and (max-width: 1024px) {
