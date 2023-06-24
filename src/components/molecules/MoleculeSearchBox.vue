beed87f4530ef37e03aa911b96430bff6df975ee
arrogant-ant <sudhanshuambastha@gmail.com>
2023-06-24 16:58:51 +0530

    booking portal UI using mock data

---
+++ src/components/molecules/MoleculeSearchBox.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="search-control">
+        <p></p>
+        <atom-input v-model="searchModel" :placeholder="placeholder">
+        </atom-input>
+        <atom-button
+            class="search-button"
+            @click.native="emitSearchEvent"
+            left="magnify"
+            >Search</atom-button
+        >
+    </div>
+</template>
+
+<script>
+import AtomInput from '../atoms/AtomInput.vue';
+import AtomButton from '../atoms/AtomButton.vue';
+export default {
+    name: 'MoleculeSearchBox',
+    components: { AtomInput, AtomButton },
+    props: {
+        placeholder: {
+            type: String,
+            default: 'Search Text',
+        },
+    },
+    data() {
+        return {
+            searchModel: '',
+        };
+    },
+    methods: {
+        emitSearchEvent() {
+            this.$emit('on-search', this.searchModel);
+        },
+    },
+};
+</script>
+
+<style>
+.search-button {
+    margin-left: 8px;
+}
+</style>
---
+++ src/views/BookingPortal.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="booking-portal-root">
+        <!--- Search Bar-->
+        <div class="search-control">
+            <p></p>
+            <molecule-search-box
+                placeholder="Booking ID"
+                @on-search="getBookingDetails"
+            ></molecule-search-box>
+        </div>
+        <template-booking-portal></template-booking-portal>
+    </div>
+</template>
+
+<script>
+import MoleculeSearchBox from '../components/molecules/MoleculeSearchBox.vue';
+import TemplateBookingPortal from '../components/templates/TemplateBookingPortal.vue';
+export default {
+    components: { MoleculeSearchBox, TemplateBookingPortal },
+    name: 'BookingPortal',
+    data() {
+        return {
+            bookingID: '',
+        };
+    },
+    methods: {
+        getBookingDetails(bookingID) {
+            console.log('booking id ::', { bookingID });
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.booking-portal-root {
+    padding: 16px;
+}
+
+.search-control {
+    display: flex;
+    justify-content: center;
+    margin: 16px;
+}
+</style>
