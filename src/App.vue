7d1935664cc0c28450cc56855a23e595ec7d9b5c
Sujeet Kumar Manjhi <sujits32@gmail.com>
2023-06-25 16:15:36 +0530

    scrollbar styled at root level

--- src/App.vue
+++ src/App.vue
@@ -47,4 +47,26 @@ export default {
 .body-container {
     padding-top: 4.375rem;
 }
+
+/* width and height of the scrollbar */
+::-webkit-scrollbar {
+    width: 4px;
+    height: 4px;
+}
+
+/* background color of the scrollbar */
+::-webkit-scrollbar-track {
+    background: #f1f1f1;
+}
+
+/* color of the scrollbar thumb */
+::-webkit-scrollbar-thumb {
+    background-color: #888;
+    border-radius: 5px;
+}
+
+/* color of the scrollbar thumb when hovering */
+::-webkit-scrollbar-thumb:hover {
+    background-color: #555;
+}
 </style>
