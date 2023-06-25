f1b556ab22adf3db9fe98bb655785c45d40cfdac
Sujeet Kumar Manjhi <sujits32@gmail.com>
2023-06-25 18:22:58 +0530

    ghost container added

--- src/components/extras/LoaderModal.vue
+++ src/components/extras/LoaderModal.vue
@@ -1,9 +1,11 @@
 <template>
-    <b-loading
-        :is-full-page="isFullPage"
-        :active="isLoading"
-        :can-cancel="false"
-    ></b-loading>
+    <div v-show="isLoading" class="ghost-container">
+        <b-loading
+            :is-full-page="isFullPage"
+            :active="isLoading"
+            :can-cancel="false"
+        ></b-loading>
+    </div>
 </template>
 
 <script>
@@ -27,6 +29,11 @@ export default {
 </script>
 
 <style lang="scss">
+.ghost-container {
+    height: 100vh;
+    background-color: #f1f1f1;
+}
+
 .body-container {
     .loading-overlay .loading-icon:after {
         border: 2px solid var(--primary-color);
