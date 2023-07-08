2c61a54ad40489bbb269ed258d7ccabdabdf7108
Sudhanshu Kr. Ambastha <sudhanshuambastha@gmail.com>
2023-07-08 17:40:34 +0530

    Use dynamic blog comp name. (#282)
    * Use dynamic blog comp name.
    
    * migrate old blogs away from json.
    
    * Two Blogs Added
    
    ---------
    
    Co-authored-by: Anuj-krSingh <warrieranu510@gmail.com>
    Co-authored-by: Sujeet Kumar Manjhi <sujits32@gmail.com>

---
+++ src/components/organisms/OrganismBlogEliminatingTraffic.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <p>
+            In the recent years all over the globe production and demand for
+            motor vehicles has increased dramatically. Earlier motor vehicles
+            were a luxury only few can afford but now their prices have
+            plummeted and its more easily available to the population. India for
+            an example took about 60 years to acquire 100 million vehicles and
+            the next 100 million vehicles were bought in the last 10 years !
+        </p>
+        <p>
+            This sheer increase in vehicles has caused a strain on the roadways
+            and parking infrastructure. Having such a high population and
+            vehicle density in India only makes things worse. These problems
+            elevate the risk of traffic jams in urban cities. With Bengaluru
+            facing the worst traffic congestion in the world, Delhi, Mumbai and
+            Pune remain in the top 10 source traffic congestion index.
+        </p>
+        <h3 class="is-size-3 has-text-weight-semibold">
+            What causes a traffic jam ?
+        </h3>
+        <p>
+            There a numerous reasons for traffic jams like lack of proper
+            infrastructure, office rush hours, illegal parkings and many more.
+            Improper parkings beside roads/no parking zones are he main cause
+            for traffic jams. Even the widest of roads have traffic jams because
+            of illegal parkings. Widening the ones which face traffic jams are
+            not so rewarding because empty roads means more room for people to
+            park vehicles. Inadequate parking infrastructure is the reason to
+            blame when people park their vehicles in no parking zones. Absence
+            of a proper inventory for parking sites and the lack of awareness is
+            the root of the cause.
+        </p>
+        <h3 class="is-size-3 has-text-weight-semibold">
+            How can a smart parking platform help ?
+        </h3>
+        <p>
+            A smart parking system allows people to search and book a secure
+            parking spot. This two features to search and book will allow people
+            to plan where they park their vehicle in a secure parking zone.
+            These two features can curb the problem of traffic jams. Car parking
+            systems are equipped with smart sensors and devices which make the
+            parking experience seamless which users will flock to experience.
+        </p>
+        <h3 class="is-size-3 has-text-weight-semibold">
+            The new normal and how ParkSpot helps the cause.
+        </h3>
+        <p>
+            The COVID-19 pandemic shed light upon the problems in our day to day
+            habits which aids in transmission of diseases. Social distancing,
+            wearing of masks and hand sanitation have become a new normal
+            withing our society. People are distancing themselves from other
+            people. ParkSpot is a smart parking platform which allows users to
+            search and book secure parking spots for long and short term.
+            ParkSpot gives users a seamless and contact-less experience which
+            users are searching for after the corona outbreak. ParkSpot also
+            allow users to list out their free parking space into our platform
+            which other users finding for a parking space can use. This will
+            help our weak parking infrastructure in the battle to curb traffic
+            congestion.
+        </p>
+        <br /><br />
+        <p>
+            Join hands with ParkSpot and help us win this fight against traffic
+            jams
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OrganismBlogEliminatingTraffic',
+};
+</script>
