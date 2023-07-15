4ae7fc4cc2844de506cb1d83ba41c052087441e1
Sudhanshu Kr. Ambastha <sudhanshuambastha@gmail.com>
2023-07-15 22:22:14 +0530

    Add updatedAt in sdp admin details (#304)
    * Add updatedAt in sdp admin details
    
    * rename method

--- src/constant/enums.js
+++ src/constant/enums.js
@@ -1,3 +1,16 @@
+/**
+ *
+ * @param {Array} enumLabels
+ * @param {int} enumValue
+ * @return {string}: enum label for enum value
+ */
+function getEnumLabel(enumLabels, enumValue) {
+    if (enumValue >= enumLabels.length) {
+        ('Undefined enum');
+    }
+    return enumLabels[enumValue];
+}
+
 export const PaymentStatus = Object.freeze({
     PaymentNotDefined: 0,
     PaymentPending: 1,
@@ -28,8 +41,41 @@ const PaymentStatusLabel = [
  * @return {string}: label for payment Status
  */
 export function getPaymentStatusLabel(paymentStatus) {
-    if (paymentStatus >= PaymentStatusLabel.length) {
-        ('Undefined Payemnt Status');
-    }
-    return PaymentStatusLabel[paymentStatus];
+    return getEnumLabel(PaymentStatusLabel, paymentStatus);
+}
+
+export const KYCStatus = Object.freeze({
+    // NotSet => KYC information NOT available
+    NotSet: 0,
+    // Pending => pending for document verification
+    Pending: 1,
+    // IDVerified => User ID card is successfully verified
+    IDVerified: 2,
+    // OwnershipVerified => User vehicle/spot ownership (depending on UserType) is successfully verified
+    OwnershipVerified: 3,
+    // Verified => all documents successfully KYC verified
+    Verified: 4,
+    // Denied => process started but non of the document was verified
+    Denied: 5,
+    // NotVerified => KYC process has not even started
+    NotVerified: 6,
+});
+
+const KYCStatusLabel = [
+    'NotSet',
+    'Pending',
+    'IDVerified',
+    'OwnershipVerified',
+    'Verified',
+    'Denied',
+    'NotVerified',
+];
+
+/**
+ *
+ * @param {int} kyStatus
+ * @return {string}: label for kyc Status
+ */
+export function getKYCStatusLabel(kyStatus) {
+    return getEnumLabel(KYCStatusLabel, kyStatus);
 }
