7af94f4121dd4eda2351bb09923d62e7d6160ceb
Sujeet Kumar Manjhi <sujits32@gmail.com>
2023-07-16 12:01:24 +0530

    logo inside loader added

--- src/components/extras/LoaderModal.vue
+++ src/components/extras/LoaderModal.vue
@@ -1,10 +1,8 @@
 <template>
-    <div v-show="isLoading" class="ghost-container">
-        <b-loading
-            :is-full-page="isFullPage"
-            :active="isLoading"
-            :can-cancel="false"
-        ></b-loading>
+    <div class="loader-container" v-if="isLoading">
+        <div class="loader">
+            <img src="../../assets/pstopmini.png" />
+        </div>
     </div>
 </template>
 
@@ -20,25 +18,64 @@ export default {
             default: false,
         },
     },
-    data() {
-        return {
-            isFullPage: true,
-        };
-    },
 };
 </script>
 
-<style lang="scss">
-.ghost-container {
+<style lang="scss" scoped>
+.loader-container {
     height: 100vh;
     background-color: #f1f1f1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 9999;
 }
 
-.body-container {
-    .loading-overlay .loading-icon:after {
-        border: 2px solid var(--primary-color);
-        border-right-color: transparent;
-        border-top-color: transparent;
+.loader {
+    border: 8px solid #f3f3f3;
+    /* Light grey */
+    border-top: 8px solid var(--primary-color);
+    /* Dark Green */
+    border-radius: 50%;
+    width: 136px;
+    height: 136px;
+    animation: spinloader 2s linear infinite;
+}
+
+.loader img {
+    height: 120px;
+    width: 120px;
+    animation: spinlogo 2s linear infinite;
+}
+
+img {
+    overflow-clip-margin: content-box;
+    overflow: clip;
+}
+
+@keyframes spinloader {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
+
+@keyframes spinlogo {
+    0% {
+        transform: rotate(360deg);
+    }
+
+    100% {
+        transform: rotate(0deg);
     }
 }
 </style>
