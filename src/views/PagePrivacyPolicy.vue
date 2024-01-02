846bfbe15dea4930973cac42be99014d7d93d0fb
Anuj-krSingh <warrieranu510@gmail.com>
2024-01-02 21:49:14 +0530

    New-Privacy-Policy-Webpage Added

---
+++ src/views/PagePrivacyPolicy.vue
@@ -0,0 +1,20 @@
+<template>
+    <TemplatePrivacyPolicy></TemplatePrivacyPolicy>
+</template>
+<script>
+import TemplatePrivacyPolicy from '../components/templates/TemplatePrivacyPolicy.vue';
+import { PAGE_TITLE } from '@/constant/constant';
+export default {
+    name: 'PagePrivacyPolicy',
+    components: {
+        TemplatePrivacyPolicy,
+    },
+    metaInfo() {
+        return {
+            title: PAGE_TITLE.PRIVACY,
+            titleTemplate: PAGE_TITLE.TITLE_TEMPLATE + '%s',
+        };
+    },
+};
+</script>
+<style></style>
