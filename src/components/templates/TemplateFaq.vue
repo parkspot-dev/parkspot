5097d50219eeb88f8cf6a0fa10fbe044f4ba4cae
Anuj-krSingh <122720966+Anuj-krSingh@users.noreply.github.com>
2024-03-27 22:12:44 +0530

    new faqlists,contact us link added & AtomParagraph used (#391)

--- src/components/templates/TemplateFaq.vue
+++ src/components/templates/TemplateFaq.vue
@@ -33,7 +33,7 @@
                         <a class="card-header-icon">
                             <b-icon
                                 :class="props.open ? 'arrow-white' : ''"
-                                :icon="props.open ? 'menu-down' : 'menu-up'"
+                                :icon="props.open ? 'menu-up' : 'menu-down'"
                             >
                             </b-icon>
                         </a>
@@ -43,17 +43,22 @@
                     <div class="content" v-html="faq.answer"></div>
                 </div>
             </b-collapse>
+            <AtomParagraph class="mt-5 is-size-5 has-text-weight-semibold">
+                In case you have more queries, please feel free to <a href="/contact">contact us.</a> 
+            </AtomParagraph>
         </div>
     </BodyWrapper>
 </template>
 
 <script>
 import AtomHeading from '../atoms/AtomHeading.vue';
+import AtomParagraph from '../atoms/AtomParagraph.vue';
 import BodyWrapper from '../extras/BodyWrapper.vue';
 export default {
     name: 'TemplateFaq',
     components: {
         AtomHeading,
+        AtomParagraph,
         BodyWrapper,
     },
     data() {
@@ -97,6 +102,7 @@ export default {
                         parking site and park your vehicle at your allocated spot without \
                         having to contact a person in person or any other mode.`,
                 },
+
                 {
                     question: `How can I rent my empty parking space?`,
                     answer: `<p>Follow below simple steps:-</p> \
@@ -112,11 +118,13 @@ export default {
                             with the request. We will contact you and process the request.\
                         </p>`,
                 },
+
                 {
                     question: `What would be the minimum duration to book a parking spot?`,
                     answer: `Depending upon the spot, the minimum duration for car and bike \
                         parking space may vary from hourly to monthly basis.`,
                 },
+
                 {
                     question: `How to make the payment for booking a parking spot?`,
                     answer: `To make the payment you can download our \
@@ -128,6 +136,7 @@ export default {
                          <a "href=https://www.parkspot.in/contact/ "target="_blank">support team, </a>\
                         they will send you a payment link using which you can make the payment.`,
                 },
+
                 {
                     question: `How to reach the booked spot location?`,
                     answer: `You can go to the My Booking option and can see your booked parking\
@@ -135,6 +144,74 @@ export default {
                          this will open the Google Maps with the location added on the Map. If \
                          the offline booking has been made, then our support team will send the \
                          parking location`,
+                },
+
+                {
+                    question: `What is the response time for all requests?`,
+                    answer: `Our dedicated sales agents respond within 12 hours to all your queries\
+                        about parking solutions in Bangalore and Hyderabad.`,
+                },
+
+                {
+                    question: `Who takes responsibility for car safety?`,
+                    answer: `Car safety is our top priority. Our parking facilities come with trained\
+                        personnel, and we deploy advanced security measures, including guards and cameras, \
+                        to ensure a secure environment.`,
+                },
+
+                {
+                    question: `How reliable is the authentication of vehicle owners?`,
+                    answer: `Rest assured, we prioritize authenticity. Our stringent KYC process for\
+                        individuals and thorough verification of car documents during the booking process\ 
+                        guarantee the reliability of vehicle owners..`,
+                },
+
+                {
+                    question: `Are spot owners charged to list their spot on the site in Parkspot?`,
+                    answer: `Absolutely not! Listing your parking spot on Parkspot is free of charge,\
+                        providing a hassle-free way to earn money by utilizing vacant spaces`,
+                },
+
+                {
+                    question: `What is the booking process for Parkspot?`,
+                    answer: `<ul>
+                                <li>Find a listing you like on ParkSpot or by contacting us.</li>\
+                                <li>Complete your registration on the portal or with the assistance 
+                                    of our sales person, we locate nearby spots.</li>\
+                                <li>Visit the parking lot and verify the facts.</li>\
+                                <li>If you like the parking, process the rent and security deposit.</li>\
+                                <li>Start parking your vehicle.</li>\
+                                <li>We will mark the parking as unavailable on ParkSpot.</li>
+                            </ul>`,
+                },
+
+                {
+                    question: `Tell us about Parkspot's smart parking solution.`,
+                    answer: `Exciting news! Our smart parking solution, which offers daily bookings, will soon be available in BTM,\
+                             Bangalore. Stay tuned for an enhanced parking experience.`,
+                },
+
+                {
+                    question: `Where does Parkspot currently operate?`,
+                    answer: `Parkspot is currently revolutionizing parking solutions in Bangalore and\
+                             Hyderabad, providing paid parking options and an opportunity for individuals\
+                             to earn money by using their vacant spaces.`,
+                },
+
+                {
+                    question: `What types of parking do you offer?`,
+                    answer: `We primarily offer monthly parking solutions, catering to long-term parking needs.\
+                             However, in some cases where spot owners allow, we also provide daily parking options.\
+                             Hourly parking is not yet available but may be introduced in the future.`,
+                },
+                
+                {
+                    question: `Do you require a security deposit for parking bookings?`,
+                    answer: `Yes, as part of our standard procedure, we collect a one-month rent deposit\
+                             for security purposes. This deposit is fully refundable and is utilized in\
+                             the final month of parking. It serves as a precautionary measure to ensure\
+                             notice periods are adhered to, allowing us time to find another booking if needed.\
+                             Additionally, in cases of delayed rent payments, the deposit is utilized to compensate the owner.`,
                 },
             ],
         };
