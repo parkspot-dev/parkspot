6fab2fb8169cb41e2e9f724782c6d526753e0428
Anuj-krSingh <warrieranu510@gmail.com>
2024-06-01 00:26:25 +0530

    SO Changes & Rent + SD new text used

--- src/constant/enums.js
+++ src/constant/enums.js
@@ -115,8 +115,8 @@ export const BookingStatusLabels = [
     'Refunded',
     'Unavailable',
     'Failed Error',
-    'Payment Pending (Rent + Conv fee due)',
-    'Visiting (Rent due)',
+    'Payment Pending (Rent + SD)',
+    'Visiting (Rent + SD)',
     'Schedule Visit (Conv fee due)',
     'Rent Due',
 ];
