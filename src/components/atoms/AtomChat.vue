c610edaa33d5c40ea7836060b053e7946879f355
Sudhanshu Kr. Ambastha <sudhanshuambastha@gmail.com>
2024-06-16 19:59:28 +0530

    Replace Fb chat with WA chat. (#418)
    * Replace Fb chat with WA chat.
    
    * Resized icon.

---
+++ src/components/atoms/AtomChat.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="chat">
+        <a
+            href="https://wa.me/917488239471?text=I'm%20interested%20in%20car%20parking."
+            target="_blank"
+        >
+            <img
+                src="../../assets/wa-icon.png"
+                alt="chat-icon"
+                class="chat-icon"
+            />
+        </a>
+    </div>
+</template>
+
+<style>
+.chat {
+    height: 64px;
+    width: 64px;
+    position: fixed;
+    right: 20px;
+    bottom: 20px;
+}
+.chat-icon {
+    transition: transform 0.2s; /* Animation */
+}
+.chat-icon:hover {
+    transform: scale(1.2); /* (120% zoom ) */
+}
+</style>
