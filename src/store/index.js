cf850da39df6e680c1a207c0b8adcdd3ed500897
Vineet Mittal <vineetrocks2000@gmail.com>
2024-11-04 23:30:07 +0530

    resolving conflicts

--- src/store/index.js
+++ src/store/index.js
@@ -9,10 +9,7 @@ import config from './config';
 import searchPortal from './searchPortal';
 import bookingPortal from './bookingPortal';
 import registerRequest from './registerRequest';
-<<<<<<< HEAD
 import reviewSpot from './reviewSpot';
-=======
->>>>>>> b410616e1fe62115dbca1db6993dd803fdf04636
 
 Vue.use(Vuex);
 
@@ -27,10 +24,7 @@ const store = new Vuex.Store({
         bookingPortal,
         config,
         registerRequest,
-<<<<<<< HEAD
         reviewSpot,
-=======
->>>>>>> b410616e1fe62115dbca1db6993dd803fdf04636
     },
 });
 
