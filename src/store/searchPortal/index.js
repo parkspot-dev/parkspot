811a496e5b64de2887ec0e82fc97cde30fdaf0d5
Saksham2k3s <sakshamshrivastav58@gmail.com>
2024-11-12 19:09:00 +0530

    fix: resolve dialog box error and add tabs to URL

--- src/store/searchPortal/index.js
+++ src/store/searchPortal/index.js
@@ -1,16 +1,26 @@
 import { mayaClient } from '@/services/api';
 
 const state = {
+    loading: false,
+    hasError: false,
+    errorMessage: '',
     activeTab: 0,
     SOLatLngInput: '',
     searchMobile: '',
     // array of objects {id, name}, both id and name has agent first name as this used in b-table filtering
     agentList: [],
+    totalRequests: [],
+    parkingRequests: [],
+    interestedVOList: [],
+    isTotalRequestFetched: false,
 };
 
 const getters = {};
 
 const mutations = {
+    'set-loading'(state) {
+        state.loading = !state.loading;
+    },
     'update-active-tab'(state, tabNo) {
         state.activeTab = tabNo;
     },
@@ -32,27 +42,110 @@ const mutations = {
             });
         state.agentList.push({ id: 'NA', name: 'NA' });
     },
-
-    'set-search-mobile'(state, text){
-        state.searchMobile = text
-    }
+    'set-total-requests'(state, requests) {
+        state.isTotalRequestFetched = true;
+        state.totalRequests = requests;
+    },
+    'set-parking-requests'(state, parkingRequests) {
+        state.parkingRequests = parkingRequests;
+    },
+    'set-interested-vo-list'(state, list) {
+        state.interestedVOList = list;
+    },
+    'set-error'(state, message) {
+        state.hasError = !state.hasError;
+        state.errorMessage = message;
+    },
+    'set-search-mobile'(state, text) {
+        state.searchMobile = text;
+    },
 };
-
 const actions = {
     updateActiveTab({ commit }, tabNo) {
         commit('update-active-tab', tabNo);
     },
-    updateSOLatLngInput({ commit }, latLng) {
-        commit('update-SO-Lat-Lng-Input', latLng);
-    },
     async getAgents({ commit }) {
         commit('set-agent-list', await mayaClient.get('/auth/user/agents'));
     },
-    
+    // Get Total Results
+    async getTotalRequests({ commit, state }) {
+        if (state.loading) return;
+        commit('set-loading', true);
+        try {
+            const parkingRequestURL = '/internal/parking-requests';
+            const response = await mayaClient.get(parkingRequestURL);
+            if (response.ErrorCode) {
+                throw new Error(response.DisplayMsg);
+            }
+            commit('set-total-requests', response);
+            commit('set-parking-requests', response);
+            commit('set-interested-vo-list', response);
+        } catch (error) {
+            commit('set-error', error.message);
+        } finally {
+            commit('set-loading', false);
+        }
+    },
+    // Get parking requests by mbile number
+    async getParkingRequests({ commit, state }, mobile) {
+        if (state.loading) return;
+        commit('set-loading', true);
+        try {
+            let parkingRequestURL = '/internal/parking-requests';
+            if (mobile) {
+                parkingRequestURL += `?mobile=${mobile.replace(/\s+/g, '')}`;
+            }
+            const response = await mayaClient.get(parkingRequestURL);
+            if (response.ErrorCode) {
+                throw new Error(response.DisplayMsg);
+            }
+            commit('set-parking-requests', response);
+        } catch (error) {
+            commit('set-error', error.message);
+        } finally {
+            commit('set-loading', false);
+        }
+    },
+    // Get Interested VO by lat lng value
+    async getInterestedVO({ commit }, latlng) {
+        commit('set-loading', true);
+        try {
+            const [lat, lng] = latlng
+                .trim()
+                .split(',')
+                .map((coord) => coord.trim());
+            const response = await mayaClient.get(
+                `/search-requests?lat=${lat}&long=${lng}`,
+            );
+            if (response.ErrorCode) {
+                throw new Error(response.DisplayMsg);
+            }
+            commit('set-interested-vo-list', response);
+        } catch (error) {
+            commit('set-error', error.message);
+        } finally {
+            commit('set-loading', false);
+        }
+    },
+    resetError({ commit }) {
+        commit('set-error', '');
+    },
+    // Reset parking requests to totalResults
+    resetParkingRequest({ commit }, list) {
+        commit('set-parking-requests', list);
+    },
+    // Reset Interested VO List to totalResults
+    resetInterestedVOList({ commit }, list) {
+        commit('set-interested-vo-list', list);
+    },
     // Update Search text with text
-    updateMobileInput({commit}, text){
-        commit('set-search-mobile', text)
-    }
+    updateMobileInput({ commit }, text) {
+        commit('set-search-mobile', text);
+    },
+    // Update SOLatLngInput
+    updateSOLatLngInput({ commit }, text) {
+        commit('update-SO-Lat-Lng-Input', text);
+    },
 };
 
 export default {
--- src/views/PageSearchPortal.vue
+++ src/views/PageSearchPortal.vue
@@ -11,7 +11,7 @@
             </div>
             <TemplateSearchPortal
                 :parkingRequests="parkingRequests"
-                :isLoading="isLoading"
+                :isLoading="loading"
                 :isSummary="true"
                 @updateRequest="updateRequest"
                 @toSrp="toSrp"
@@ -22,11 +22,13 @@
             <p></p>
             <MoleculeSearchBox
                 placeholder="Lat,Long"
-                @on-search="getInterestedVO"
+                :initialValue="SOLatLngInput"
+                @on-search="searchRequestWithLatLng"
+                @clear-input="onClearLatLngInput"
             ></MoleculeSearchBox>
             <TemplateSearchPortal
-                :isLoading="isLoading"
-                :parkingRequests="intrestedVOList"
+                :isLoading="loading"
+                :parkingRequests="interestedVOList"
                 @toSrp="toSrp"
                 @updateRequest="updateRequest"
             ></TemplateSearchPortal>
@@ -54,9 +56,7 @@ export default {
     },
     data() {
         return {
-            parkingRequests: [],
             isLoading: false,
-            intrestedVOList: [],
             VOMobile: this.searchMobile,
         };
     },
@@ -65,6 +65,13 @@ export default {
             'activeTab',
             'SOLatLngInput',
             'searchMobile',
+            'totalRequests',
+            'parkingRequests',
+            'interestedVOList',
+            'loading',
+            'errorMessage',
+            'hasError',
+            'isTotalRequestFetched',
         ]),
         activeTabView: {
             get() {
@@ -74,7 +81,7 @@ export default {
                 const currentTab =
                     tabNo === 0 ? 'parking-request' : 'interested-request';
                 this.$router.push({
-                    path: this.$route.fullPath,
+                    path: this.$route.path,
                     query: { tab: currentTab },
                 });
                 this.updateActiveTab(tabNo);
@@ -90,24 +97,38 @@ export default {
         },
     },
     async created() {
-        const currentTab =
-            this.activeTab === 0 ? 'parking-request' : 'interested-request';
-        this.$router.push({
-            path: this.$route.fullPath,
-            query: { tab: currentTab },
-        });
-        this.getAgents();
-        const mobile = this.$route.query['mobile'];
-        if (mobile) {
-            this.updateMobileInput(mobile);
+        if (
+            !this.isTotalRequestFetched &&
+            Object.keys(this.$route.query).length === 1
+        ) {
+            this.getTotalRequests();
+        }
+        const tab = this.$route.query['tab'];
+        if (tab) {
+            if (tab === 'interested-request') {
+                this.updateActiveTab(1);
+                if (this.$route.query['lat'] && this.$route.query['lng']) {
+                    const lat = this.$route.query['lat'];
+                    const lng = this.$route.query['lng'];
+                    this.updateSOLatLngInput(lat + ',' + lng);
+                    this.getInterestedVO(this.SOLatLngInput);
+                }
+            } else {
+                this.updateActiveTab(0);
+                const mobile = this.$route.query['mobile'];
+                if (mobile) {
+                    this.updateMobileInput(mobile);
+                }
+                this.getParkingRequests(this.$route.query['mobile']);
+            }
         } else {
-            this.updateMobileInput('');
+            this.updateActiveTab(0);
+            this.$router.push({
+                path: this.$route.fullPath,
+                query: { tab: 'parking-request' },
+            });
         }
-
-        this.getParkingRequests(this.$route.query['mobile']);
-        if (this.SOLatLngInput) {
-            this.getInterestedVO(this.SOLatLngInput);
-        }
+        this.getAgents();
     },
     methods: {
         ...mapActions('searchPortal', [
@@ -115,6 +136,12 @@ export default {
             'updateSOLatLngInput',
             'getAgents',
             'updateMobileInput',
+            'getParkingRequests',
+            'resetError',
+            'getInterestedVO',
+            'getTotalRequests',
+            'resetParkingRequest',
+            'resetInterestedVOList',
         ]),
         alertError(msg) {
             this.$buefy.dialog.alert({
@@ -129,7 +156,28 @@ export default {
                 // This will hamper interested VO section experience,
                 // because interested VO does not change the URL and
                 // reload the page will take to /search-portal
-                onConfirm: this.onClearMobileInput,
+                onConfirm: () => {
+                    // Make Mobile Input Empty
+                    if (this.searchMobile) {
+                        this.updateMobileInput('');
+                    }
+                    // Make LatLngInput to empty
+                    if (this.SOLatLngInput) {
+                        this.updateSOLatLngInput('');
+                    }
+                    // Check for current tab
+                    const tab =
+                        this.activeTab === 0
+                            ? 'parking-request'
+                            : 'interested-request';
+                    // Reset Error
+                    this.resetError();
+                    // Push Back
+                    this.$router.push({
+                        name: 'SearchPortal',
+                        query: { tab: tab },
+                    });
+                },
             });
         },
         async searchRequestWithMobile(voMobile) {
@@ -143,46 +191,38 @@ export default {
                 this.getParkingRequests(voMobile);
             }
         },
+        async searchRequestWithLatLng(latlng) {
+            const location = latlng.trim().split(',');
+            const lat = location[0].trim();
+            const lng = location[1].trim();
+            this.$router.push({
+                path: this.$route.fullPath,
+                query: { lat: lat, lng: lng },
+            });
+            this.getInterestedVO(lat + ',' + lng);
+        },
         // Clear Mobile Input
         async onClearMobileInput() {
             if (this.$route.query.mobile) {
                 this.updateMobileInput('');
-                this.$router.push({ name : 'SearchPortal' });
-                // Fetch the Agents Data
-                this.getAgents();
+                this.$router.replace({ name: 'SearchPortal' });
+                if (this.totalRequests) {
+                    this.resetParkingRequest(this.totalRequests);
+                }
             }
         },
-
-        async getParkingRequests(voMobile = '') {
-            this.isLoading = true;
-            let parkingRequestURL = '/internal/parking-requests';
-            if (voMobile != '') {
-                parkingRequestURL =
-                    parkingRequestURL +
-                    `?mobile=${voMobile.replace(/\s+/g, '')}`;
+        // Clear LatLng Input
+        async onClearLatLngInput() {
+            if (this.$route.query['lat'] && this.$route.query['lng']) {
+                this.updateSOLatLngInput('');
+                this.$router.replace({
+                    name: 'SearchPortal',
+                    query: { tab: 'interested-request' },
+                });
+                if (this.totalRequests) {
+                    this.resetInterestedVOList(this.totalRequests);
+                }
             }
-            const response = await mayaClient.get(parkingRequestURL);
-            this.isLoading = false;
-            if (response.ErrorCode) {
-                this.alertError(response.DisplayMsg);
-                return;
-            }
-            this.parkingRequests = response;
-        },
-        async getInterestedVO(latlng) {
-            this.isLoading = true;
-            const location = latlng.trim().split(',');
-            const lat = location[0].trim();
-            const lng = location[1].trim();
-            const parkingRequestList = await mayaClient.get(
-                `/search-requests?lat=${lat}&long=${lng}`,
-            );
-            this.isLoading = false;
-            if (parkingRequestList.ErrorCode) {
-                this.alertError(parkingRequestList.DisplayMsg);
-                return;
-            }
-            this.intrestedVOList = parkingRequestList;
         },
         async updateRequest(request) {
             try {
@@ -204,7 +244,6 @@ export default {
                 console.error({ error });
                 this.alertError('Something went wrong!');
             }
-
             this.isLoading = false;
         },
         toSrp(lat, lng) {
@@ -216,6 +255,13 @@ export default {
                 },
             });
             window.open(routeData.href, '_blank');
+        },
+    },
+    watch: {
+        hasError(error) {
+            if (error) {
+                this.alertError(this.errorMessage);
+            }
         },
     },
 };
