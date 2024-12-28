80c0bf39705b07b655e1431759e8dcd7100b2ba7
Saksham2k3s <sakshamshrivastav58@gmail.com>
2024-12-28 21:23:32 +0530

    get map access token from firebase

--- src/components/extras/MapContainer.vue
+++ src/components/extras/MapContainer.vue
@@ -5,6 +5,7 @@
 <script>
 import mapboxgl from 'mapbox-gl';
 import { mapGetters, mapMutations } from 'vuex';
+import { getValueFromFirebase } from '../../firebase';
 export default {
     name: 'MapContainer',
     props: {
@@ -22,7 +23,7 @@ export default {
     emits: ['location'],
     data() {
         return {
-            accessToken: process.env.VUE_APP_MAP_ACCESS_TOKEN,
+            accessToken: null,
             img: require('@/assets/pstopmini.png'),
             map: null, // map for mapbox
             marker: null, // marker for location
@@ -39,126 +40,154 @@ export default {
             this.recenterMap(newCenter);
         },
     },
+
     mounted() {
-        mapboxgl.accessToken = this.accessToken;
+                this.getMapAccessToken().then(() => {
+                    if (this.accessToken) {
+                        mapboxgl.accessToken = this.accessToken;
 
-        // create map
-        this.map = new mapboxgl.Map(this.mapConfig);
+                        // Create map
+                        this.map = new mapboxgl.Map(this.mapConfig);
 
-        // create the popup
-        const popup = new mapboxgl.Popup({ offset: 25 }).setText(
-            'Your current location.',
-        );
+                        // Create popup
+                        const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+                            'Your current location.',
+                        );
 
-        // create the marker
-        this.marker = new mapboxgl.Marker({
-            draggable: this.drag,
-        })
-            .setLngLat(this.mapConfig.center)
-            .setPopup(popup)
-            .addTo(this.map);
+                        // Create marker
+                        this.marker = new mapboxgl.Marker({
+                            draggable: this.drag,
+                        })
+                            .setLngLat(this.mapConfig.center)
+                            .setPopup(popup)
+                            .addTo(this.map);
 
-        if (this.drag) {
-            this.map.on('click', (e) => {
-                this.marker.setPopup(popup).setLngLat(e.lngLat).addTo(this.map);
-                this.updateMapConfig(this.marker.getLngLat());
-                this.$emit('location', this.marker.getLngLat());
-            });
+                        if (this.drag) {
+                            this.map.on('click', (e) => {
+                                this.marker
+                                    .setPopup(popup)
+                                    .setLngLat(e.lngLat)
+                                    .addTo(this.map);
+                                this.updateMapConfig(this.marker.getLngLat());
+                                this.$emit('location', this.marker.getLngLat());
+                            });
 
-            this.marker.on('dragend', () => {
-                this.updateMapConfig(this.marker.getLngLat());
-                this.$emit('location', this.marker.getLngLat());
-            });
-        }
+                            this.marker.on('dragend', () => {
+                                this.updateMapConfig(this.marker.getLngLat());
+                                this.$emit('location', this.marker.getLngLat());
+                            });
+                        }
 
-        // create DOM element for the parking site marker
-        for (const spot of this.spotsList) {
-            const encodedSpotId = encodeURIComponent(spot.ID);
-            const psMarker = document.createElement('div');
+                        // Add parking site markers
+                        for (const spot of this.spotsList) {
+                            const encodedSpotId = encodeURIComponent(spot.ID);
+                            const psMarker = document.createElement('div');
 
-            psMarker.className = 'marker';
-            psMarker.style.backgroundImage = 'url(' + this.img + ')';
-            psMarker.style.width = '50px';
-            psMarker.style.height = '50px';
-            psMarker.style.backgroundSize = '110%';
+                            psMarker.className = 'marker';
+                            psMarker.style.backgroundImage = 'url(' + this.img + ')';
+                            psMarker.style.width = '50px';
+                            psMarker.style.height = '50px';
+                            psMarker.style.backgroundSize = '110%';
 
-            const psPopup = new mapboxgl.Popup({ offset: 25 }).setHTML(
-                `
-<style>
-    .name {
-        font-weight: bold;
-        font-size: 1rem;
-    }
+                            const psPopup = new mapboxgl.Popup({ offset: 25 }).setHTML(
+                                `
+        <style>
+            .name {
+                font-weight: bold;
+                font-size: 1rem;
+            }
 
-    .address {
-        font-size: 0.75rem;
-    }
+            .address {
+                font-size: 0.75rem;
+            }
 
-    .btn-container {
-        align-content: space-around;
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        margin-top: 1rem;
+            .btn-container {
+                align-content: space-around;
+                display: flex;
+                flex-direction: row;
+                flex-wrap: wrap;
+                margin-top: 1rem;
 
-    }
+            }
 
-    .btn {
-        align-items: center;
-        background-color: #ffe08a;
-        border-radius: 8px;
-        border-color: #dbdbdb;
-        border-width: 2px;
-        cursor: pointer;
-        font-size: .75rem;
-        font-weight: 700;
-        justify-content: center;
-        margin:4px;
-        padding-bottom: calc(.5em - 1px);
-        padding-left: 1em;
-        padding-right: 1em;
-        padding-top: calc(.5em - 1px);
-        text-align: center;
-        white-space: nowrap;
-    }
-</style>
-<div>
-    <div class="name">${spot.Name}</div>
-    <div class="address"> ${spot.Address}
-        <div>India</div>
-    </div>
-</div>
-<div style="margin-top:.5rem"><b>Distance: </b> ${spot.Distance} Km</div>
-<div><b>Rent: </b> &#8377; ${spot.Rate}/- </div>
-<div class="btn-container">
-    <a href="https://www.google.com/maps/search/?api=1&query=${spot.Lat},${spot.Long}" target="_blank">
-  <button class="btn">
-     Navigate 
-  </button>
-</a>
-    <a href="https://www.parkspot.in/spot-details/${encodedSpotId}" target="_blank">
-  <button class="btn">
-     View Spot 
-  </button>
-</a>
-</div>`,
-            );
+            .btn {
+                align-items: center;
+                background-color: #ffe08a;
+                border-radius: 8px;
+                border-color: #dbdbdb;
+                border-width: 2px;
+                cursor: pointer;
+                font-size: .75rem;
+                font-weight: 700;
+                justify-content: center;
+                margin:4px;
+                padding-bottom: calc(.5em - 1px);
+                padding-left: 1em;
+                padding-right: 1em;
+                padding-top: calc(.5em - 1px);
+                text-align: center;
+                white-space: nowrap;
+            }
+        </style>
+        <div>
+            <div class="name">${spot.Name}</div>
+            <div class="address"> ${spot.Address}
+                <div>India</div>
+            </div>
+        </div>
+        <div style="margin-top:.5rem"><b>Distance: </b> ${spot.Distance} Km</div>
+        <div><b>Rent: </b> &#8377; ${spot.Rate}/- </div>
+        <div class="btn-container">
+            <a href="https://www.google.com/maps/search/?api=1&query=${spot.Lat},${spot.Long}" target="_blank">
+          <button class="btn">
+             Navigate
+          </button>
+        </a>
+            <a href="https://www.parkspot.in/spot-details/${encodedSpotId}" target="_blank">
+          <button class="btn">
+             View Spot
+          </button>
+        </a>
+        </div>`,
+                            );
 
-            new mapboxgl.Marker(psMarker)
-                .setLngLat([spot.Long, spot.Lat])
-                .setPopup(psPopup)
-                .addTo(this.map);
-        }
+                            new mapboxgl.Marker(psMarker)
+                                .setLngLat([spot.Long, spot.Lat])
+                                .setPopup(psPopup)
+                                .addTo(this.map);
+                        }
+                    }
+                });
     },
+
     methods: {
         ...mapMutations({
             updateMapConfig: 'map/update-map-config',
         }),
         recenterMap(center) {
+            if (!this.map) {
+                return;
+            }
+
             this.map.flyTo({
                 center: center,
             });
             this.marker.setLngLat(center);
+        },
+
+        async getMapAccessToken() {
+            const keys = await getValueFromFirebase(
+                `/app-config/mapbox-access-data`,
+            );
+
+            // Check if we are getting keys from firebase
+            if (keys && keys.length > 0) {
+                const randomIndex = Math.floor(Math.random() * keys.length);
+                const selectedKey = keys[randomIndex];
+                this.accessToken = selectedKey.token;
+            } else {
+                console.error("Error while fetching map access token");
+            }
         },
     },
 };
