e619c112729e3b698f6e128ba536f69c34af7524
Saksham2k3s <sakshamshrivastav58@gmail.com>
2025-01-02 20:13:36 +0530

    Added getPsPopup method

--- src/components/extras/MapContainer.vue
+++ src/components/extras/MapContainer.vue
@@ -60,54 +60,109 @@ export default {
             this.marker.setLngLat(center);
         },
         renderMap() {
-            if (this.accessToken) {
-                mapboxgl.accessToken = this.accessToken;
+            if (!this.accessToken) {
+                return;
+            }
+            mapboxgl.accessToken = this.accessToken;
 
-                // Create map
-                this.map = new mapboxgl.Map(this.mapConfig);
+            // Create map
+            this.map = new mapboxgl.Map(this.mapConfig);
 
-                // Create popup
-                const popup = new mapboxgl.Popup({ offset: 25 }).setText(
-                    'Your current location.',
+            // Create popup
+            const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+                'Your current location.',
+            );
+
+            // Create marker
+            this.marker = new mapboxgl.Marker({
+                draggable: this.drag,
+            })
+                .setLngLat(this.mapConfig.center)
+                .setPopup(popup)
+                .addTo(this.map);
+
+            if (this.drag) {
+                this.map.on('click', (e) => {
+                    this.marker
+                        .setPopup(popup)
+                        .setLngLat(e.lngLat)
+                        .addTo(this.map);
+                    this.updateMapConfig(this.marker.getLngLat());
+                    this.$emit('location', this.marker.getLngLat());
+                });
+
+                this.marker.on('dragend', () => {
+                    this.updateMapConfig(this.marker.getLngLat());
+                    this.$emit('location', this.marker.getLngLat());
+                });
+            }
+
+            // Add parking site markers
+            for (const spot of this.spotsList) {
+                const encodedSpotId = encodeURIComponent(spot.ID);
+                const psMarker = document.createElement('div');
+
+                psMarker.className = 'marker';
+                psMarker.style.backgroundImage = 'url(' + this.img + ')';
+                psMarker.style.width = '50px';
+                psMarker.style.height = '50px';
+                psMarker.style.backgroundSize = '110%';
+
+                const psPopup = this.getPsPopup(spot, encodedSpotId);
+                new mapboxgl.Marker(psMarker)
+                    .setLngLat([spot.Long, spot.Lat])
+                    .setPopup(psPopup)
+                    .addTo(this.map);
+            }
+        },
+
+        // Get map access token from cookies
+        getMapAccessTokenFromCookie() {
+            const cookies = document.cookie.split(';');
+            for (const cookie of cookies) {
+                let [key, value] = cookie.split('=');
+                key = key.trim();
+                if (key === 'mapAccessToken') {
+                    return value;
+                }
+            }
+            return null;
+        },
+
+        // Set map access token in cookies
+        setMapAccessTokenInCookies(mapAccessToken) {
+            const date = new Date();
+            date.setTime(date.getTime() + 15 * 24 * 60 * 60 * 1000);
+            const expires = `expires=${date.toUTCString()}`;
+            document.cookie = `mapAccessToken=${mapAccessToken}; ${expires}; path=/`;
+            this.accessToken = mapAccessToken;
+        },
+
+        async getMapAccessToken() {
+            let mapAccessToken = this.getMapAccessTokenFromCookie();
+            if (mapAccessToken) {
+                // If map access token is present in cookie
+                this.accessToken = mapAccessToken;
+            } else {
+                // If not present in cookie
+                const keys = await getValueFromFirebase(
+                    `/app-config/mapbox-access-data`,
                 );
 
-                // Create marker
-                this.marker = new mapboxgl.Marker({
-                    draggable: this.drag,
-                })
-                    .setLngLat(this.mapConfig.center)
-                    .setPopup(popup)
-                    .addTo(this.map);
-
-                if (this.drag) {
-                    this.map.on('click', (e) => {
-                        this.marker
-                            .setPopup(popup)
-                            .setLngLat(e.lngLat)
-                            .addTo(this.map);
-                        this.updateMapConfig(this.marker.getLngLat());
-                        this.$emit('location', this.marker.getLngLat());
-                    });
-
-                    this.marker.on('dragend', () => {
-                        this.updateMapConfig(this.marker.getLngLat());
-                        this.$emit('location', this.marker.getLngLat());
-                    });
+                // Check if we are getting keys from firebase
+                if (keys && keys.length > 0) {
+                    const randomIndex = Math.floor(Math.random() * keys.length);
+                    const selectedKey = keys[randomIndex];
+                    mapAccessToken = selectedKey.token;
+                    setMapAccessTokenInCookies(mapAccessToken);
+                } else {
+                    console.error('Error while fetching map access token');
                 }
+            }
+        },
 
-                // Add parking site markers
-                for (const spot of this.spotsList) {
-                    const encodedSpotId = encodeURIComponent(spot.ID);
-                    const psMarker = document.createElement('div');
-
-                    psMarker.className = 'marker';
-                    psMarker.style.backgroundImage = 'url(' + this.img + ')';
-                    psMarker.style.width = '50px';
-                    psMarker.style.height = '50px';
-                    psMarker.style.backgroundSize = '110%';
-
-                    const psPopup = new mapboxgl.Popup({ offset: 25 }).setHTML(
-                        `
+        getPsPopup(spot, encodedSpotId) {
+            const popupHTML = `
         <style>
             .name {
                 font-weight: bold;
@@ -165,60 +220,9 @@ export default {
              View Spot
           </button>
         </a>
-        </div>`,
-                    );
+        </div>`;
 
-                    new mapboxgl.Marker(psMarker)
-                        .setLngLat([spot.Long, spot.Lat])
-                        .setPopup(psPopup)
-                        .addTo(this.map);
-                }
-            }
-        },
-        
-        // Get map access token from cookies
-        getMapAccessTokenFromCookie() {
-            const cookies = document.cookie.split(';');
-            for (const cookie of cookies) {
-                let [key, value] = cookie.split('=');
-                key = key.trim();
-                if (key === 'mapAccessToken') {
-                    return value;
-                }
-            }
-            return null;
-        },
-
-        // Set map access token in cookies
-        setMapAccessTokenInCookies(mapAccessToken){
-            const date = new Date();
-            date.setTime(date.getTime() + 15 * 24 * 60 * 60 * 1000);
-            const expires = `expires=${date.toUTCString()}`;
-            document.cookie = `mapAccessToken=${mapAccessToken}; ${expires}; path=/`;
-            this.accessToken = mapAccessToken;
-        },
-
-        async getMapAccessToken() {
-            let mapAccessToken = this.getMapAccessTokenFromCookie();
-            if (mapAccessToken) {
-                // If map access token is present in cookie
-                this.accessToken = mapAccessToken;
-            } else {
-                // If not present in cookie
-                const keys = await getValueFromFirebase(
-                    `/app-config/mapbox-access-data`,
-                );
-
-                // Check if we are getting keys from firebase
-                if (keys && keys.length > 0) {
-                    const randomIndex = Math.floor(Math.random() * keys.length);
-                    const selectedKey = keys[randomIndex];
-                    mapAccessToken = selectedKey.token;
-                    setMapAccessTokenInCookies(mapAccessToken);
-                } else {
-                    console.error('Error while fetching map access token');
-                }
-            }
+            return new mapboxgl.Popup({ offset: 25 }).setHTML(popupHTML);
         },
     },
 };
