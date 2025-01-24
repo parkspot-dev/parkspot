f28521695b5ca862afe8fd3bb2c7bebe6335d13a
Vineet Mittal <vineetrocks2000@gmail.com>
2025-01-24 16:04:46 +0530

    Improving CSS for swiper

--- src/components/organisms/OrganismTestimonialSwiper.vue
+++ src/components/organisms/OrganismTestimonialSwiper.vue
@@ -26,12 +26,12 @@
 </template>
 
 <script>
-import SwiperCore, { Navigation, Pagination } from 'swiper';
+import SwiperCore, { Navigation, Pagination, Autoplay  } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import MoleculeTestimonialCard from '@/components/molecules/MoleculeTestimonialCard.vue';
 import 'swiper/swiper-bundle.min.css';
 
-SwiperCore.use([Navigation, Pagination]);
+SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 export default {
   name: 'TestimonialSwiper',
@@ -85,73 +85,90 @@ export default {
           spaceBetween: 40,
         },
       },
+      autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+      },
     };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.testimonial-swiper-container {
-    position: relative;
+$swiper-padding: 2.5em 1.5em;
+$button-size: 20px;
+$button-color: #000;
+$inactive-opacity: 0.2;
+$active-transform: scale(1.1);
 
-    .swiper {
-        padding: 2.5em 1.5em;
-        height: 700px;
+.swiper {
+  padding: $swiper-padding;
+  height: 700px;
 
-        .swiper-slide {
-            margin: 0;
-            height: auto;
-            width: 100%;
-            padding: 0;
-            opacity: 0.2;
-            background-color: rgba(255, 255, 255, 0.33);
-            border-radius: 6px;
-            transition: all 0.5s ease-in-out;
+  .swiper-slide {
+    margin: 0;
+    height: auto;
+    width: 100%;
+    padding: 0;
+    opacity: $inactive-opacity;
+    background: rgba(255, 255, 255, 0.33);
+    border-radius: 6px;
+    transition: opacity 0.3s ease, transform 0.5s ease-in-out;
 
-            &.swiper-slide-active {
-                background-color: rgba(255, 255, 255, 1);
-                opacity: 1;
-                transform: scale(1.1);
-            }
-        }
+    &.swiper-slide-active {
+      background: #fff;
+      opacity: 1;
+      transform: $active-transform;
+    }
+  }
 
-        .swiper-nav-wrapper {
-            position: absolute;
-            bottom: -30px; 
-            left: 50%;
-            transform: translateX(-50%);
-            display: flex;
-            justify-content: center;
-            align-items: center;
+  .swiper-nav-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 1em;
 
-            .swiper-button-next,
-            .swiper-button-prev {
-                width: 20px;
-                height: 20px;
-                background-size: contain; 
-                background-repeat: no-repeat;
+    .swiper-button-next,
+    .swiper-button-prev {
+      width: $button-size;
+      height: $button-size;
+      background-size: cover;
+      background-repeat: no-repeat;
+      margin: 0 1em;
+      cursor: pointer;
+      transition: transform 0.3s;
 
-                &:after {
-                    display: none; 
-                }
-            }
-        }
+      &:hover {
+        transform: scale(1.2);
+      }
+    }
+
+    .swiper-button-next {
+      background-image: url("data:image/svg+xml,%0A%3Csvg width='9px' height='16px' viewBox='0 0 9 16' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3Cg id='Page-1' stroke='none' stroke-width='1' fill='none' fill-rule='evenodd'%3E%3Cg id='chevron-right' fill='%23000000' fill-rule='nonzero'%3E%3Cpath d='M8.674805,7.066406 L1.924805,0.316406 C1.696288,0.105468 1.432619,0 1.133789,0 C0.834959,0 0.57129,0.105468 0.342773,0.316406 C0.114257,0.544923 0,0.808592 0,1.107422 C0,1.406251 0.114257,1.669921 0.342773,1.898438 L6.301758,7.857422 L0.342773,13.816406 C0.114257,14.044923 0,14.308592 0,14.607422 C0,14.906251 0.114257,15.169921 0.342773,15.398438 C0.465821,15.521485 0.584472,15.609375 0.69873,15.662109 C0.812989,15.714844 0.958007,15.741211 1.133789,15.741211 C1.309571,15.741211 1.454589,15.714844 1.568848,15.662109 C1.683106,15.609375 1.801757,15.521485 1.924805,15.398438 L8.674805,8.648438 C8.903321,8.419921 9.017578,8.156251 9.017578,7.857422 C9.017578,7.558592 8.903321,7.294923 8.674805,7.066406 Z' id='Path'%3E%3C/path%3E%3C/g%3E%3C/g%3E%3C/svg%3E") !important;
+    }
+
+    .swiper-button-prev {
+      background-image: url("data:image/svg+xml,%0A%3Csvg width='9px' height='16px' viewBox='0 0 9 16' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3Cg id='Page-1' stroke='none' stroke-width='1' fill='none' fill-rule='evenodd'%3E%3Cg id='chevron-right' transform='translate(4.508789, 7.870605) rotate(-180.000000) translate(-4.508789, -7.870605) translate(-0.000000, -0.000000)' fill='%23000000' fill-rule='nonzero'%3E%3Cpath d='M8.674805,7.066406 L1.924805,0.316406 C1.696288,0.105468 1.432619,0 1.133789,0 C0.834959,0 0.57129,0.105468 0.342773,0.316406 C0.114257,0.544923 0,0.808592 0,1.107422 C0,1.406251 0.114257,1.669921 0.342773,1.898438 L6.301758,7.857422 L0.342773,13.816406 C0.114257,14.044923 0,14.308592 0,14.607422 C0,14.906251 0.114257,15.169921 0.342773,15.398438 C0.465821,15.521485 0.584472,15.609375 0.69873,15.662109 C0.812989,15.714844 0.958007,15.741211 1.133789,15.741211 C1.309571,15.741211 1.454589,15.714844 1.568848,15.662109 C1.683106,15.609375 1.801757,15.521485 1.924805,15.398438 L8.674805,8.648438 C8.903321,8.419921 9.017578,8.156251 9.017578,7.857422 C9.017578,7.558592 8.903321,7.294923 8.674805,7.066406 Z' id='Path'%3E%3C/path%3E%3C/g%3E%3C/g%3E%3C/svg%3E") !important;
     }
 
     .swiper-pagination {
-        position: relative !important;
-        display: flex; 
-        justify-content: center; 
+      display: flex;
+      gap: 0.5em;
+      justify-content: center;
 
-        .swiper-pagination-bullet {
-            background-color: var(--parkspot-white) !important;
-            transition: all 0.2s ease-in-out;
+      .swiper-pagination-bullet {
+        width: 12px;
+        height: 12px;
+        background: #ccc;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: background 0.3s;
 
-            &.swiper-pagination-bullet-active {
-                background-color: var(--secondary-color);
-                transform: scale(1.5); 
-            }
+        &-active {
+          background: #000;
         }
+      }
     }
+  }
 }
 </style>
