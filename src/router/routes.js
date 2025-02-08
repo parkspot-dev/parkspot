4b97a93baa5e830ebbe5d4225e0069ebae60cd75
Saksham2k3s <sakshamshrivastav58@gmail.com>
2025-02-08 10:57:34 +0530

    correct payment route

--- src/router/routes.js
+++ src/router/routes.js
@@ -17,7 +17,7 @@ export const pages = {
     BLOG                    : '/blog',
     MAINBLOG                : '/blog/:id',
     SEARCH_PORTAL           : '/search-portal',
-    PAYMENTGATEWAY          : '/payment/:transactionId',
+    PAYMENTGATEWAY          : '/payment/:pathMatch(.*)*',
     NEARBY                  : '/bangalore/parking-near-:location',
     NEARBY_HYD              : '/hyderabad/parking-near-:location',
     TEMP                    : '/temp',
