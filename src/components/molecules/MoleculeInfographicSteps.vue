65b1b5c8ac29a128e59b06075c8f8af0581bba64
Saksham2k3s <sakshamshrivastav58@gmail.com>
2025-02-16 20:35:00 +0530

    fix infographic css at sdp page

--- src/components/molecules/MoleculeInfographicSteps.vue
+++ src/components/molecules/MoleculeInfographicSteps.vue
@@ -3,40 +3,37 @@
         <div class="infographic-bg-img"></div>
         <div class="infographic-first steps-container">
             <div class="step-img"></div>
-            <AtomImage src="/assets/first.png" alt="find" />
+            <img src="/assets/first.png" alt="find" />
             <p>Find a listing you like on ParkSpot.</p>
         </div>
         <div class="infographic-second steps-container">
-            <AtomImage src="/assets/second.png" alt="find" />
+            <img src="/assets/second.png" alt="find" />
             <p>Complete your registration.</p>
         </div>
         <div class="infographic-third steps-container">
-            <AtomImage src="/assets/third.png" alt="find" />
+            <img src="/assets/third.png" alt="find" />
             <p>Visit the parking and verify the facts.</p>
         </div>
         <div class="infographic-fourth steps-container">
-            <AtomImage src="/assets/fourth.png" alt="find" />
+            <img src="/assets/fourth.png" alt="find" />
             <p>
                 If you like the parking, process the rent and security deposit.
             </p>
         </div>
         <div class="infographic-fifth steps-container">
-            <AtomImage src="/assets/fifth.png" alt="find" />
+            <img src="/assets/fifth.png" alt="find" />
             <p>Start parking your vehicle.</p>
         </div>
         <div class="infographic-sixth steps-container">
-            <AtomImage src="/assets/sixth.png" alt="find" />
+            <img src="/assets/sixth.png" alt="find" />
             <p>We will mark the parking as unavailable on ParkSpot.</p>
         </div>
     </div>
 </template>
 
 <script>
-import AtomImage from '../atoms/AtomImage.vue';
-
 export default {
     name: 'InfographicSteps',
-    components: { AtomImage },
     data() {
         return {
             bgImage: `url('/assets/infographic-line.png')`,
@@ -71,6 +68,11 @@ $right-pos: 735px;
             background-image: none;
             height: 20px;
         }
+    }
+
+    img{
+        height: 120px;
+        width: 120px;
     }
 
     p {
