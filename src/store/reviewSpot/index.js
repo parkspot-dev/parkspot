ebe931c9fdd832f525f86e5047df6e8b73ba6f68
Vineet Mittal <vineetrocks2000@gmail.com>
2025-03-16 22:37:07 +0530

    correcting regex

--- src/store/reviewSpot/index.js
+++ src/store/reviewSpot/index.js
@@ -137,8 +137,11 @@ const actions = {
         } else {
             const urlPattern = new RegExp(
                 '^(https?:\\/\\/)?' + // protocol
-                '((([a-zA-Z\\d]([a-zA-Z\\d-]*[a-zA-Z\\d])*)\\.)+[a-zA-Z]{2,}|' + // domain name
-                '(\\:\\d+)?(\\/[-a-zA-Z\\d%_.~+]*)*' + // port and path
+                '((([a-zA-Z\\d]([a-zA-Z\\d-]*[a-zA-Z\\d])*)\\.)+[a-zA-Z]{2,}' + // domain name
+                '|localhost' + // localhost
+                '|\\d{1,3}(\\.\\d{1,3}){3})' + // IPv4 address
+                '(\\:\\d+)?' + // port
+                '(\\/[-a-zA-Z\\d%_.~+]*)*' + // path
                 '(\\?[;&a-zA-Z\\d%_.~+=-]*)?' + // query string
                 '(\\#[-a-zA-Z\\d_]*)?$', // fragment locator
                 'i'
