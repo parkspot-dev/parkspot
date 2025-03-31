bf1ae2ea1cd23915d173133491a469f2ea54de25
Vineet Mittal <vineetrocks2000@gmail.com>
2025-03-31 17:41:13 +0530

    Testing on uat Search -portal

--- src/components/extras/NavbarBody.vue
+++ src/components/extras/NavbarBody.vue
@@ -49,6 +49,35 @@
                                 </ul>
                             </div>
                         </li>
+                        <li class="menu-item" v-if="isAgent">
+                            <span class="menu-title">Agent Portal</span>
+                            <!-- menu dropdown -->
+                            <div class="menu-item-dropdown">
+                                <ul>
+                                    <li>
+                                        <router-link
+                                            :to="{ name: 'SearchPortal' }"
+                                        >
+                                            Search Portal
+                                        </router-link>
+                                    </li>
+                                    <li>
+                                        <router-link
+                                            :to="{ name: 'booking-portal' }"
+                                        >
+                                            Bookings
+                                        </router-link>
+                                    </li>
+                                    <li>
+                                        <router-link
+                                            :to="{ name: 'spotRequest' }"
+                                        >
+                                            Spot Requests
+                                        </router-link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
                         <li class="menu-item">
                             <router-link
                                 class="menu-link"
@@ -379,11 +408,11 @@ export default {
             activeSlide: 0,
         };
     },
-
     computed: {
         ...mapState('user', {
             user: (state) => state.user,
             isAuthReady: (state) => state.isAuthReady,
+            isAgent: (state) => state.isAgent,
         }),
         ...mapState('config', ['helplineNumber', 'helplineRef']),
     },
@@ -391,9 +420,7 @@ export default {
         ...mapMutations('user', {
             updateLoginModal: 'update-login-Modal',
         }),
-        ...mapActions('user', {
-            logOut: 'logOut',
-        }),
+        ...mapActions('user', ['logOut', 'getUserProfile']),
 
         logInBtn() {
             this.updateLoginModal(true);
@@ -419,6 +446,9 @@ export default {
         backToMainScroll() {
             this.activeSlide = 0; // making main slide active
         },
+    },
+    mounted() {
+        this.getUserProfile();
     },
 };
 </script>
