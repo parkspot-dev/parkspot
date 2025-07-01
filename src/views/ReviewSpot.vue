b3857e384934ade6965b7d08e269673e02fcb65a
Saksham2k3s <sakshamshrivastav58@gmail.com>
2025-07-01 19:25:42 +0530

    rebase master branch

--- src/views/ReviewSpot.vue
+++ src/views/ReviewSpot.vue
@@ -668,7 +668,6 @@ export default {
              const FacilitiesName = this.SO.Facilities.map((facility) => {
               return facility.Name;
            })
-
            return JSON.stringify(FacilitiesName) !== JSON.stringify(this.Facilities);
            }
 
